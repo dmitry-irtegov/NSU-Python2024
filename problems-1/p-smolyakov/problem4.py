78a999aad97343133843e5f61d7b4c0041de4f31
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2024-02-12 00:06:57 +0700

    problem4 done

---
+++ problems-1/p-smolyakov/problem4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import os
+import os.path
+from functools import reduce
+
+if __name__ == '__main__':
+    filenames = list(filter(os.path.isfile, os.listdir(path='.')))
+    sizes = list(map(lambda filename: os.stat(os.path.join('.', filename)).st_size, filenames))
+
+    for filename, size in sorted(zip(filenames, sizes), key=lambda x: (-x[1], x[0])):
+        print(f'{filename}: {size} B')
