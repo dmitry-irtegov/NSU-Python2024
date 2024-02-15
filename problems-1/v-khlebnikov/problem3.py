ce9dc4787abd18f0944424491ba8be872267cb88
vhlebnikov <v.khlebnikov@g.nsu.ru>
2024-02-16 02:33:27 +0700

    Problem-1-3 done

---
+++ problems-1/v-khlebnikov/problem3.py
@@ -0,0 +1,14 @@
+def collatz_hypothesis(x):
+    print(x, end=" ")
+    while x > 1:
+        print("->", end=" ")
+        if x % 2 == 0:
+            x /= 2
+        else:
+            x = 3 * x + 1
+        print(x, end=" ")
+
+
+if __name__ == '__main__':
+    x = input()
+    collatz_hypothesis(int(x))
