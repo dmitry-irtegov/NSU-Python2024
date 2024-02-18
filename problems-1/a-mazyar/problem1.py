1abc03a046f716e1a405706e2ba1fa7e5c83fdbc
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-02-18 16:57:20 +0700

    Add floats and alt signs test

--- problems-1/a-mazyar/problem1.py
+++ problems-1/a-mazyar/problem1.py
@@ -1,4 +1,5 @@
 import unittest
+from numpy.testing import assert_almost_equal
 
 def cumulative_sum(array):
     c_arr = [0] * (len(array) + 1)
@@ -7,12 +8,18 @@ def cumulative_sum(array):
 
     return c_arr
 
-class TestCumulativeSumFunction(unittest.TestCase):
+class TestCumulativeSum(unittest.TestCase):
     def test_example(self):
         self.assertEqual(cumulative_sum([1, 2, 3]), [0, 1, 3, 6])
 
     def test_negative_numbers(self):
         self.assertEqual(cumulative_sum([-1, -2, -3]), [0, -1, -3, -6])
+
+    def test_alt_signs(self):
+        self.assertEqual(cumulative_sum([-1, 0, 1]), [0, -1, -1, 0])
+
+    def test_floats(self):
+        assert_almost_equal(cumulative_sum([-1.2, 0.3, 1.4]), [0, -1.2, -0.9, 0.5])
 
     def test_empty_input(self):
         self.assertEqual(cumulative_sum([]), [0])
