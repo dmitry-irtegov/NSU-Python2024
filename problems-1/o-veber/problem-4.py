96e6771066d6b4d65e0858e61cfa19154ac965bc
Fagor <oleg-veber@mail.ru>
2024-02-19 21:45:21 +0700

    Change izvracheniy to basics

--- problems-1/o-veber/problem-4.py
+++ problems-1/o-veber/problem-4.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    bottle_count = ['Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two', 'One']
+    bottle_count = ['Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two', 'One', 'no']
     bottle_fall = '{} one green bottle should accidentally fall,'
     will_be = '{}{} green {} hanging on the wall{}'
     nothing = ''
@@ -7,9 +7,7 @@ if __name__ == '__main__':
     for i in range(10):
         condition = "And if" if i != 9 else "If that"
         counts = "bottles" if i != 9 else "bottle"
-        next_bottle_count = bottle_count[i + 1] if i != 9 else 'no'
-        current_bottle_count = bottle_count[i]
-        print(will_be.format(nothing, current_bottle_count, counts, comma))
-        print(will_be.format(nothing, current_bottle_count, counts, comma))
+        print(will_be.format(nothing, bottle_count[i], counts, comma))
+        print(will_be.format(nothing, bottle_count[i], counts, comma))
         print(bottle_fall.format(condition))
-        print(will_be.format('There’ll be ', str.lower(next_bottle_count), counts, '.'))
+        print(will_be.format('There’ll be ', str.lower(bottle_count[i + 1]), counts, '.'))
