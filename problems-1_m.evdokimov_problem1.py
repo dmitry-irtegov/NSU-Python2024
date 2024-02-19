97dad26454a8469588d9c08ca466641adc200770
Mikhail <evdokimm05@gmail.com>
2024-02-19 11:46:41 +0700

    optimisation

--- problems-1_m.evdokimov_problem1.py
+++ problems-1_m.evdokimov_problem1.py
@@ -1,6 +1,10 @@
 inputList = input().split()
 
-resultList = [0]
-for i in range(len(inputList)):
-    resultList.append(resultList[i] + int(inputList[i]))
+l = len(inputList)
+resultList = [None] * (l + 1)
+resultList[0] = 0
+
+for i in range(l):
+    resultList[i+1] = resultList[i] + int(inputList[i])
+    
 print(resultList)
