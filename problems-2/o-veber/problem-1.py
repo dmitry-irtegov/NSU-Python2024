1a94aa0894c97033e2e41469ef31a16f5d561654
Fagor <oleg-veber@mail.ru>
2024-02-19 23:57:07 +0700

    Change test names

--- problems-2/o-veber/problem-1.py
+++ problems-2/o-veber/problem-1.py
@@ -1,25 +1,26 @@
 import unittest
 
 
-def get_pithagor_triples(digit):
+def get_pythagore_triples(digit):
     return [(x, y, z) for x in range(0, digit + 1)
             for y in range(0, digit + 1)
             for z in range(0, digit + 1)
             if x ** 2 + y ** 2 == z ** 2]
 
 
-class TestSong(unittest.TestCase):
+class TestPythagore(unittest.TestCase):
+
     def test_negative_case(self):
-        self.assertEqual([], get_pithagor_triples(-100))
+        self.assertEqual([], get_pythagore_triples(-100))
 
     def test_zero_case(self):
-        self.assertEqual([(0, 0, 0)], get_pithagor_triples(0))
+        self.assertEqual([(0, 0, 0)], get_pythagore_triples(0))
 
     def test_one_case(self):
-        self.assertEqual([(0, 0, 0), (0, 1, 1), (1, 0, 1)], get_pithagor_triples(1))
+        self.assertEqual([(0, 0, 0), (0, 1, 1), (1, 0, 1)], get_pythagore_triples(1))
 
     def test_two_case(self):
-        self.assertEqual([(0, 0, 0), (0, 1, 1), (0, 2, 2), (1, 0, 1), (2, 0, 2)], get_pithagor_triples(2))
+        self.assertEqual([(0, 0, 0), (0, 1, 1), (0, 2, 2), (1, 0, 1), (2, 0, 2)], get_pythagore_triples(2))
 
 
 if __name__ == "__main__":
