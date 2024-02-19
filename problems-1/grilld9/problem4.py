a99a7e8cd618daa24e41c99f8c0d6eeef1605470
Maxim <112863178+grilld9@users.noreply.github.com>
2024-02-19 11:03:44 +0600

    capitalize added

--- problems-1/grilld9/problem4.py
+++ problems-1/grilld9/problem4.py
@@ -1,12 +1,11 @@
 templ_wall = " green bottles hanging on the wall"
 templ_fall = "And if one green bottle should accidentally fall,\n"
 templ_will = "There’ll be "
-amounts = ["Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One", "no"]
-amounts_low_case = ["ten", "nine", "eight", "seven", "six", "five", "four", "three", "two", "one", "no"]
+amounts = ["ten", "nine", "eight", "seven", "six", "five", "four", "three", "two", "one", "no"]
 result = ""
-for i in range(0, 10):
-    result += amounts[i] + templ_wall + ",\n"
-    result += amounts[i] + templ_wall + ",\n"
+for i in range(10):
+    result += amounts[i].capitalize() + templ_wall + ",\n"
+    result += amounts[i].capitalize() + templ_wall + ",\n"
     result += templ_fall
-    result += templ_will + amounts_low_case[i + 1] + templ_wall + ".\n"
+    result += templ_will + amounts[i + 1] + templ_wall + ".\n"
 print(result)
