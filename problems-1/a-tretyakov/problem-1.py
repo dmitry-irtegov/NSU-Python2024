f3b86523b471e0915655cc45f56a85cc5041eb05
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2024-02-19 14:13:46 +0700

    problems-1/task-1: added unit tests

--- problems-1/a-tretyakov/problem-1.py
+++ problems-1/a-tretyakov/problem-1.py
@@ -1,13 +1,24 @@
 from functools import reduce
+import unittest
 
 
 def cumulative_sum(data):
-    if not data:
-        raise Exception("Number sequence is empty")
-    else:
-        return f"Your list: {reduce(lambda acc, x: [*acc, acc[-1] + x], data, [0])}"
+    return reduce(lambda acc, x: [*acc, acc[-1] + x], data, [0])
+
+
+class TestCumulativeSum(unittest.TestCase):
+    def test_example_list(self):
+        self.assertEqual([0, 1, 3, 6], cumulative_sum([1, 2, 3]))
+
+    def test_empty_list(self):
+        self.assertEqual([0], cumulative_sum([]))
+
+    def test_zero_list(self):
+        self.assertEqual([0, 0, 0, 0], cumulative_sum([0, 0, 0]))
+
+    def test_negative_numbers_list(self):
+        self.assertEqual([0, -1, -3, -6], cumulative_sum([-1, -2, -3]))
 
 
 if __name__ == "__main__":
-    my_list = list(map(int, input().split()))
-    print(cumulative_sum(my_list))
+    unittest.main()
