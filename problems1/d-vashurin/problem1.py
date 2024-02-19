dfe501eb264509392a78a181d06f0bd34827efab
kapkekes <kapkekes@yandex.ru>
2024-02-20 02:39:55 +0700

    Use `TypeVar` instead of union type

--- problems1/d-vashurin/problem1.py
+++ problems1/d-vashurin/problem1.py
@@ -1,6 +1,8 @@
 from collections.abc import Iterable
+from typing import TypeVar
 
+N = TypeVar("N", int, float, int | float)
 
-def cumulative_sums(nums: Iterable[int | float | complex]) -> list[int | float | complex]:
-    current: int | float | complex = 0
+def cumulative_sums(nums: Iterable[N]) -> list[N]:
+    current: N = 0
     return [(current := current + n) for n in nums]
