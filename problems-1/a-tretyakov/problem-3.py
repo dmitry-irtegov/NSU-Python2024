542cebb2540677779c04e79528a33f2b4d39c283
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2024-02-19 14:56:45 +0700

    added tests

--- problems-1/a-tretyakov/problem-3.py
+++ problems-1/a-tretyakov/problem-3.py
@@ -1,16 +1,36 @@
+import unittest
+
+
 def collatz_hypothesis(num, chain=None):
     if chain is None:
         chain = [num]
 
     if num == 0:
-        return "Number doesn't satisfies Collatz hypothesis"
+        raise ValueError("Number doesn't satisfies Collatz hypothesis")
     if num == 1:
-        return f"Transformation chain of the Collatz hypothesis: {chain}"
+        return chain
 
     num = num // 2 if num % 2 == 0 else num * 3 + 1
     return collatz_hypothesis(num, [*chain, num])
 
 
+class TestCollatzHypothesis(unittest.TestCase):
+
+    def test_simple_case(self):
+        self.assertEquals([3, 10, 5, 16, 8, 4, 2, 1], collatz_hypothesis(3))
+
+    def test_big_number(self):
+        self.assertEquals(
+            [9, 28, 14, 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1],
+            collatz_hypothesis(9)
+        )
+
+    def test_one(self):
+        self.assertEquals([1], collatz_hypothesis(1))
+
+    def test_zero(self):
+        self.assertRaises(ValueError, collatz_hypothesis, 0)
+
+
 if __name__ == "__main__":
-    number = int(input())
-    print(collatz_hypothesis(number))
+    unittest.main()
