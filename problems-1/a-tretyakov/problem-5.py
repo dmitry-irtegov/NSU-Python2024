10c4b29d21283ec7e3f0c3c7629cb9cb8f9f002f
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2024-02-19 15:24:43 +0700

    added tests

--- problems-1/a-tretyakov/problem-5.py
+++ problems-1/a-tretyakov/problem-5.py
@@ -1,8 +1,11 @@
+import unittest
+
+
 def factorize_number(num):
     if num == 1:
         return [1, 1]
     elif num < 1:
-        raise Exception("Number must be positive")
+        raise ValueError("Number must be positive")
 
     prime = 2
     factor_list = []
@@ -22,6 +25,20 @@ def factorize_number(num):
     return factor_list
 
 
+class TestFactorizeNumber(unittest.TestCase):
+
+    def test_factorize_simple_number(self):
+        self.assertEquals([[2, 1], [3, 2], [7, 1]], factorize_number(126))
+
+    def test_factorize_prime_degree(self):
+        self.assertEquals([[5, 4]], factorize_number(625))
+
+    def test_factorize_negative(self):
+        self.assertRaises(ValueError, factorize_number, -1)
+
+    def test_factorize_zero(self):
+        self.assertRaises(ValueError, factorize_number, 0)
+
+
 if __name__ == "__main__":
-    number = int(input())
-    print(factorize_number(number))
+    unittest.main()
