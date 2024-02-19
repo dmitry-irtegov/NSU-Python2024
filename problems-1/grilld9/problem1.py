f9b17832385ae7af1859512558168d1b3a130620
Maxim <112863178+grilld9@users.noreply.github.com>
2024-02-19 10:42:59 +0600

    fixed size array

--- problems-1/grilld9/problem1.py
+++ problems-1/grilld9/problem1.py
@@ -1,10 +1,12 @@
 def cumulative_sums(nums):
     sum = 0
-    result = [sum]
-    for num in nums:
+    result = [0] * (len(nums) + 1)
+    result[0] = sum
+    for idx, num in enumerate(nums):
         sum += num
-        result.append(sum)
+        result[idx + 1] = sum
     return result
+
 
 ex_input = [1, 2, 3]
 print("Ex1 output:", cumulative_sums(ex_input))
