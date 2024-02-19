a89fc1ea9b476337ef9225e88f9a3e95b42056aa
inikta <iniktus@mail.ru>
2024-02-19 17:16:51 +0700

    another one fix

--- problems-1/n-batenko/problems-1_n-batenko_task-5.py
+++ problems-1/n-batenko/problems-1_n-batenko_task-5.py
@@ -5,19 +5,22 @@ def get_primes_powers_naive(n):
     num = n
 
     def is_prime(num, primes):
-        for i in range(2, int(sqrt(num)) + 1, 2):
+        k = 1
+        for i in range(2, int(sqrt(num)) + 1, k):
             if num in primes:
                 return True
             else:
                 if num % i == 0:
                     return False
+            if i == 3:
+                k += 1
         return True
     
     result = []
     primes = []
     if not is_prime(num, primes):
         k = 1
-        for factor in range(2, num // 2 + 1, k):  #make it correct
+        for factor in range(2, num // 2 + 1, k):
             if (not is_prime(factor, primes)):
                 continue
             power = 0
