94b769938cf2bc13a6ccb380c5e604d00b6f9a00
kapkekes <kapkekes@yandex.ru>
2024-02-20 02:36:23 +0700

    Use `TypeVar` instead of union type

--- problems1/d-vashurin/problem2.py
+++ problems1/d-vashurin/problem2.py
@@ -1,7 +1,9 @@
 from collections.abc import MutableSequence
+from typing import TypeVar
 
+N = TypeVar("N", int, float, int | float)
 
-def bound_seq(nums: MutableSequence[int | float], lower: int | float, upper: int | float) -> None:
+def bound_seq(nums: MutableSequence[N], lower: N, upper: N) -> None:
     if lower > upper:
         message = f"{lower = } is greater than {upper =}"
         raise ValueError(message)
