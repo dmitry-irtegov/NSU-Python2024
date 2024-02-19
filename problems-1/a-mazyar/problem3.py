f20c2dcee3236fef1af1c9d0216e31a443a46284
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-02-19 14:57:53 +0700

    Add simple unittest

--- problems-1/a-mazyar/problem3.py
+++ problems-1/a-mazyar/problem3.py
@@ -1,14 +1,16 @@
+import unittest
+
 def get_collatz_list(n):
     res = [n]
     while True:
+        if n <= 1:
+            break
         if(n%2 == 0):
             n = n // 2
         else:
             n = 3*n + 1
 
         res.append(n)
-        if n == 1:
-            break
 
     return res
 
@@ -20,6 +22,12 @@ def print_arrows(arr):
 def print_collatz(n):
     print_arrows(get_collatz_list(n))
 
+class TestCollatzCheck(unittest.TestCase):
+    def test_simple_examples(self):
+        self.assertEqual(get_collatz_list(5), [5,16,8,4,2,1])
+        self.assertEqual(get_collatz_list(0), [0])
+
 if __name__ == '__main__':
     n = int(input())
     print_arrows(get_collatz_list(n))
+    unittest.main()
