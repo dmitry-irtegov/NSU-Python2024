8025befebae2f3f60044afa07797adc33fc1daaf
Мазяр Алексей Николаевич <94759449+tobaffoon@users.noreply.github.com>
2024-02-19 17:29:17 +0700

    Problem1_3 (#45)
    * Add collatz sequence function
    
    * Replace recursion with loop, separate print functionality
    
    * Add simple unittest
    
    * Replace list with tuple

---
+++ problems-1/a-mazyar/problem3.py
@@ -0,0 +1,33 @@
+import unittest
+
+def get_collatz_list(n):
+    res = (n, )
+    while True:
+        if n <= 1:
+            break
+        if(n%2 == 0):
+            n = n // 2
+        else:
+            n = 3*n + 1
+
+        res += (n, )
+
+    return res
+
+def print_arrows(arr):
+    for x in arr[:-1]:
+        print(x, end=" -> ")
+    print(arr[-1])
+
+def print_collatz(n):
+    print_arrows(get_collatz_list(n))
+
+class TestCollatzCheck(unittest.TestCase):
+    def test_simple_examples(self):
+        self.assertEqual(get_collatz_list(5), (5,16,8,4,2,1))
+        self.assertEqual(get_collatz_list(0), (0,))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_arrows(get_collatz_list(n))
+    unittest.main()
