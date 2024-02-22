3ccb685266f19bd21d07c3f66bac32d064fe1018
SSKaZZDom <91606261+SSKaZZDom@users.noreply.github.com>
2024-02-22 09:08:24 +0700

    Delete problems-1 directory

--- problems-1/o-veber/problem-2.py
+++
@@ -1,7 +0,0 @@
-
-# modifies input array
-def bound(array, lower_bound, upper_bound):
-    for i in range(len(array)):
-        array[i] = array[i] if array[i] > lower_bound else lower_bound
-        array[i] = array[i] if array[i] < upper_bound else upper_bound
-    return array
