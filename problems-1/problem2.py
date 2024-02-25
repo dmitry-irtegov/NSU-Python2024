53b3d1b0ce274ba382cb3a10c725d89234306671
chiguaua <91546000+chiguaua@users.noreply.github.com>
2024-02-26 01:33:26 +0700

    problem 2

---
+++ problems-1/problem2.py
@@ -0,0 +1,20 @@
+import unittest
+
+def cut_fun(numbers, min_border, max_border):
+    for i, v in enumerate(numbers):
+        numbers[i] = min(max(v, min_border), max_border)
+    return numbers
+
+
+class TestCalculator(unittest.TestCase):
+    def test_normal(self):
+        self.assertEqual(cut_fun([1, 2, 3], 1, 3), [1, 2, 3])
+    def test_min(self):
+        self.assertEqual(cut_fun([1, 2, 3], 2, 3), [2, 2, 3])
+    def test_max(self):
+        self.assertEqual(cut_fun([1, 2, 3], 1, 2), [1, 2, 2])
+    def test_ficha(self):
+        self.assertEqual(cut_fun([1, 2, 3], 3, 2), [2, 2, 2])
+
+if __name__ == "__main__":
+  unittest.main()
