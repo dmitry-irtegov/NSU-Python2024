c8d5d880be9c75ca80c143cee195a56fccec4011
chiguaua <91546000+chiguaua@users.noreply.github.com>
2024-02-26 02:35:03 +0700

    problem3

---
+++ problems-1/problem3.py
@@ -0,0 +1,31 @@
+import unittest
+
+def collatz_row(n):
+    row = [n]
+    while (n > 1):
+        if(n%2 == 1):
+            n = 3*n + 1
+            row.append(n)
+        n = n//2
+        row.append(n)
+    return row
+
+def get_row_string(row):
+    ans = str(row.pop(0))
+    for i in row:
+        ans += " -> " + str(i)
+    return ans
+
+n = int(input())
+print(get_row_string(collatz_row(n)))
+
+###
+    
+class TestCollatzRow(unittest.TestCase):
+    def test_examples(self):
+        self.assertEqual(collatz_row(3), [3, 10, 5, 16, 8, 4, 2, 1])
+    def test(self):
+        self.assertEqual(collatz_row(4), [4, 2, 1])
+
+if __name__ == '__main__':
+    unittest.main()
