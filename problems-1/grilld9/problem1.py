437c3bfa6641d88112834e32324918342e0c16fe
Maxim <112863178+grilld9@users.noreply.github.com>
2024-02-26 11:25:48 +0700

    unit tests

--- problems-1/grilld9/problem1.py
+++ problems-1/grilld9/problem1.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def cumulative_sums(nums):
     sum = 0
     result = [0] * (len(nums) + 1)
@@ -8,5 +11,12 @@ def cumulative_sums(nums):
     return result
 
 
-ex_input = [1, 2, 3]
-print("Ex1 output:", cumulative_sums(ex_input))
+class TestCumulativeSums(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(cumulative_sums([1, 2, 3]), [0, 1, 3, 6])
+        self.assertEqual(cumulative_sums([5, 6, 1]), [0, 5, 11, 12])
+
+
+if '__main__' == __name__:
+    unittest.main()
