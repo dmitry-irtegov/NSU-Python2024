6c6f0605afadcddaa021a22a790edb8df4f564c1
Maxim <112863178+grilld9@users.noreply.github.com>
2024-02-26 11:38:59 +0700

    unit tests

--- problems-1/grilld9/problem2.py
+++ problems-1/grilld9/problem2.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def seq_cut(nums, a, b):
     for idx, num in enumerate(nums):
         if num < a:
@@ -6,5 +9,13 @@ def seq_cut(nums, a, b):
             nums[idx] = b
     return nums
 
-ex_input = [1, 2, 3]
-print("Ex2 output:", seq_cut(ex_input, 2, 2))
+
+class TestCumulativeSums(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(seq_cut([1, 2, 3], 2, 2), [2, 2, 2])
+        self.assertEqual(seq_cut([-5, 1334, 3221, 543], 1600, 3000), [1600, 1600, 3000, 1600])
+
+
+if '__main__' == __name__:
+    unittest.main()
