30a9dab034f712dcd71de39d04cd24e341dd6c13
coffe <a.potapova8@g.nsu.ru>
2024-02-26 15:14:07 +0700

    exception handling

--- problems-2/a-potapova/problem3.py
+++ problems-2/a-potapova/problem3.py
@@ -8,13 +8,19 @@ def get_files(directory_path: str):
     for i, filename in enumerate(filenames):
         size = os.stat(directory_path + '/' + filename).st_size
         directory_files[i] = (filename, size)
-    return sorted(directory_files, key=lambda x: -x[1])
+    directory_files.sort(key=lambda x: (-x[1], x[0]))
+    return directory_files
 
 
 if __name__ == '__main__':
-    path = sys.argv[1]
-    print(f"current directory: {path}")
-    files = get_files(path)
-    print(f"{'NAME':<50}  {'SIZE (in bytes)':<4}")
-    for file in files:
-        print(f"{file[0]:<50}  {file[1]:<4}")
+    try:
+        if len(sys.argv) != 2:
+            raise ValueError('Incorrect number of arguments')
+        path = sys.argv[1]
+        print(f"current directory: {path}")
+        files = get_files(path)
+        print(f"{'NAME':<50}  {'SIZE (in bytes)':<4}")
+        for file in files:
+            print(f"{file[0]:<50}  {file[1]:<4}")
+    except (ValueError, NotADirectoryError) as e:
+        print(e)
