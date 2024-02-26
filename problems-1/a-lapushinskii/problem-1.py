c5e4f67fc21ca86b1ec3d969b22f35851b55bf2a
chiguaua <91546000+chiguaua@users.noreply.github.com>
2024-02-26 11:15:13 +0700

    test

--- problems-1/a-lapushinskii/problem-1.py
+++ problems-1/a-lapushinskii/problem-1.py
@@ -6,3 +6,17 @@ def sum_cumulative (arr):
     return result
 
 print(sum_cumulative([1, 2, 3]))
+
+###
+
+import unittest
+
+class TestCumulativeSum(unittest.TestCase):
+    def test_examples(self):
+        self.assertEqual(sum_cumulative([1, 2, 3]), [0, 1, 3, 6])
+    def test_more(self):
+        self.assertEqual(sum_cumulative([1, 2, 3, 4, 5, 6, 7]), [0, 1, 3, 6, 10, 15, 21, 28])
+
+
+if __name__ == '__main__':
+    unittest.main()
