606f3ad26dc5de35af9d9c53ac8eee42f895c83a
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2024-02-26 17:24:40 +0700

    fixed word postfix

--- problems-1/a-tretyakov/problem-4.py
+++ problems-1/a-tretyakov/problem-4.py
@@ -13,18 +13,22 @@ numbers_dict = {
 }
 
 
+def get_string_postfix(word_end):
+    return f"green bottle{word_end} hanging on the wall"
+
+
 def ten_green_bottles_lyrics(count=10):
     if count == 0:
         return
 
-    word_end = "" if count == 1 else "s"
-    phrase_postfix = f"green bottle{word_end} hanging on the wall"
+    first_word_end = "" if count == 1 else "s"
+    second_word_end = "" if count == 2 else "s"
 
     for i in range(2):
-        print(f"{numbers_dict[count]} {phrase_postfix},")
+        print(f"{numbers_dict[count]} {get_string_postfix(first_word_end)},")
 
     print("And if one green bottle should accidentally fall,")
-    print(f"There’ll be {numbers_dict[count - 1].lower()} {phrase_postfix}.")
+    print(f"There’ll be {numbers_dict[count - 1].lower()} {get_string_postfix(second_word_end)}.")
 
     ten_green_bottles_lyrics(count - 1)
 
