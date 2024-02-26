b3d352c48b8f93d1b751bc0f53b59c7e4b995446
chiguaua <91546000+chiguaua@users.noreply.github.com>
2024-02-26 11:46:42 +0700

    improve test

--- problems-1/a-lapushinskii/problem2.py
+++ problems-1/a-lapushinskii/problem2.py
@@ -15,6 +15,8 @@ class TestCutFunction(unittest.TestCase):
         self.assertEqual(cut_fun([1, 2, 3], 1, 2), [1, 2, 2])
     def test_ficha(self):
         self.assertEqual(cut_fun([1, 2, 3], 3, 2), [2, 2, 2])
+    def test_zero(self):
+        self.assertEqual(cut_fun([], 3, 2), [])
 
 if __name__ == "__main__":
   unittest.main()
