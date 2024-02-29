196d01190683dd6e99be21279754d904dcf31d44
kapkekes <kapkekes@yandex.ru>
2024-02-29 21:05:58 +0700

    Add tests for task 1 from set 1

--- problems1/d-vashurin/problem1.py
+++ problems1/d-vashurin/problem1.py
@@ -1,8 +1,36 @@
 from collections.abc import Iterable
-from typing import TypeVar
+from itertools import chain
+from typing import TYPE_CHECKING
 
-N = TypeVar("N", int, float, int | float)
+if TYPE_CHECKING:
+    from typing import TypeVar
 
-def cumulative_sums(nums: Iterable[N]) -> list[N]:
-    current: N = 0
-    return [(current := current + n) for n in nums]
+    from _typeshed import SupportsAdd
+
+    A = TypeVar("A", bound=SupportsAdd)
+
+
+def cumulative_sums(nums: "Iterable[A]") -> "list[A]":
+    """Returns the cumulative sum of the `nums` iterable.
+
+    >>> cumulative_sums((1, 2, 3))
+    [1, 3, 6]
+
+    >>> cumulative_sums(range(10, 0, -2))
+    [10, 18, 24, 28, 30]
+
+    >>> cumulative_sums([])
+    []
+
+    >>> cumulative_sums(["Hello", ", ", "World", "!"])
+    ['Hello', 'Hello, ', 'Hello, World', 'Hello, World!']
+    """
+
+    iterator = iter(nums)
+
+    try:
+        current = next(iterator)
+    except StopIteration:
+        return []
+    else:
+        return list(chain([current], ((current := current + a) for a in iterator)))
