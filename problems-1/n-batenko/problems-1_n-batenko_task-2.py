5ceec6a70b9af25c747da8fdd7a343bcb1c9c408
Inikta <iniktus@mail.ru>
2024-02-29 17:55:26 +0700

    little test fix

--- problems-1/n-batenko/problems-1_n-batenko_task-2.py
+++ problems-1/n-batenko/problems-1_n-batenko_task-2.py
@@ -17,15 +17,15 @@ def substitution_function(seq, a, b):
 class SubstiotutionTests(unittest.TestCase):
     def test_equals_dif(self):
         print(substitution_function([0, 0, 0], 1, 5))
-        self.assertEqual(substitution_function([0, 0, 0], 1, 5) == [1, 1, 1])
+        self.assertEqual(substitution_function([0, 0, 0], 1, 5), [1, 1, 1])
     
     def test_dif_equals(self):
         print(substitution_function([-100, 10, 100], -99, 99))
-        self.assertEqual(substitution_function([-100, 10, 100], -99, 99) == [-99, 10, 99])
+        self.assertEqual(substitution_function([-100, 10, 100], -99, 99), [-99, 10, 99])
 
     def test_equals_equals(self):
         print(substitution_function([0, 0, 0], 0, 0))
-        self.assertEqual(substitution_function([0, 0, 0], 0, 0) == [0, 0, 0])
+        self.assertEqual(substitution_function([0, 0, 0], 0, 0), [0, 0, 0])
 
 if __name__ == '__main__':
     unittest.main()
