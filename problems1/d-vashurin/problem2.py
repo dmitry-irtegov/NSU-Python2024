afd4a9874d31bbaa56e3fc0876c9ae5e1c62b7a8
kapkekes <kapkekes@yandex.ru>
2024-03-01 00:55:36 +0700

    Add tests for task 2 from set 1

--- problems1/d-vashurin/problem2.py
+++ problems1/d-vashurin/problem2.py
@@ -1,15 +1,48 @@
 from collections.abc import MutableSequence
-from typing import TypeVar
+from typing import TYPE_CHECKING, TypeVar
 
-N = TypeVar("N", int, float, int | float)
+if TYPE_CHECKING:
+    from _typeshed import SupportsDunderGT, SupportsDunderLT
 
-def bound_seq(nums: MutableSequence[N], lower: N, upper: N) -> None:
+    Ordered = TypeVar("Ordered", SupportsDunderGT, SupportsDunderLT)
+
+
+def bound_seq(
+    seq: "MutableSequence[Ordered]",
+    lower: "Ordered",
+    upper: "Ordered",
+) -> "MutableSequence[Ordered]":
+    """Bound the provided mutable `seq` with the `lower` and `upper` bounds.
+
+    >>> bound_seq([1.0, 2.4, 1.9, 5.6], 2.0, 3.0)
+    [2.0, 2.4, 2.0, 3.0]
+    >>> bound_seq(["Orange", "Apple", "Ginger", "Tomato", "Lemon", "Pumpkin"], "Cucumber", "Potato")
+    ['Orange', 'Cucumber', 'Ginger', 'Potato', 'Lemon', 'Potato']
+    >>> bound_seq([242, 29450], 100000, 104)
+    Traceback (most recent call last):
+        ...
+    ValueError: lower = 100000 is greater than upper = 104
+
+    All elements are changed in the following order:
+    - if the element is lower than `lower`, it will be replaced with `lower`;
+    - else if the element is greater than `upper`, it will be replaced with `upper`;
+    - else the element remains in its place.
+
+    Note:
+        `lower` should be less than `upper`.
+
+    Raises:
+        ValueError:
+            If `lower` is greater than `upper`.
+    """
     if lower > upper:
-        message = f"{lower = } is greater than {upper =}"
+        message = f"{lower = } is greater than {upper = }"
         raise ValueError(message)
 
-    for index, num in enumerate(nums):
-        if num < lower:
-            nums[index] = num
-        elif upper < num:
-            nums[index] = upper
+    for index, elem in enumerate(seq):
+        if elem < lower:
+            seq[index] = lower
+        elif upper < elem:
+            seq[index] = upper
+
+    return seq
