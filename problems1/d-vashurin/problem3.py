f0ffe28834d6e21202b435ad2916c6c18a37be1c
kapkekes <kapkekes@yandex.ru>
2024-03-01 16:44:29 +0700

    Add tests for task 3 from set 1

--- problems1/d-vashurin/problem3.py
+++ problems1/d-vashurin/problem3.py
@@ -1,11 +1,19 @@
 from collections.abc import Iterator
 
 
-def collatz_conjecture(start: int) -> Iterator[int]:
+def _collatz_iter(start: int) -> Iterator[int]:
     yield start
 
     while start != 1:
         yield (start := (3 * start + 1 if start % 2 else start // 2))
+
+
+def collatz_conjecture(start: int) -> Iterator[int]:
+    if start <= 0:
+        message = "The Collatz conjecture is defined only for positive integers"
+        raise ValueError(message)
+
+    return _collatz_iter(start)
 
 
 def main() -> None:
