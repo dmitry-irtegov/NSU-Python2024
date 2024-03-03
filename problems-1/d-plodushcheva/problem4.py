7820f4f3d9ed7a550162318a431aaedd3f740d95
ploddasha <ploddasha@yandex.ru>
2024-03-03 23:46:00 +0700

    less code, if in print

--- problems-1/d-plodushcheva/problem4.py
+++ problems-1/d-plodushcheva/problem4.py
@@ -1,22 +1,14 @@
 
 
 def ten_green_bottles_song():
-    numbers = ["ten", "nine", "eight", "seven", "six", "five", "four", "three", "two", "one", "no"]
-    s1 = "green bottles hanging on the wall,"
-    s2 = "And if one green bottle should accidentally fall,"
-    s3 = "There’ll be green bottles hanging on the wall."
-    for i in range(9):
-        print(numbers[i].title(), s1)
-        print(numbers[i].title(), s1)
-        print(s2)
-        if i == 8:
-            print(s3[:11], numbers[i + 1], s3[12:24], s3[26:])
-        else:
-            print(s3[:11], numbers[i + 1], s3[12:])
-    print(numbers[9].title(), " ", s1[:12], s1[13:], sep='')
-    print(numbers[9].title(), " ", s1[:12], s1[13:], sep='')
-    print("If that", s2[7:])
-    print(s3[:11], numbers[10], s3[12:])
+    nums = ["ten", "nine", "eight", "seven", "six", "five", "four", "three", "two", "one", "no"]
+    for i in range(10):
+        for _ in range(2):
+            print(nums[i].title() + " green bottle" + ("s" if i != 9 else "")
+                  + " hanging on the wall")
+        print("And if one" + " green bottle" + " should accidentally fall")
+        print("There’ll be " + nums[i + 1] + " green bottle" +
+              ("s" if i != 8 else "") + " hanging on the wall")
 
 
 if __name__ == '__main__':
