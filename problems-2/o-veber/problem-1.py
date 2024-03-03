003d5fbb38967d391c0d12eae01d4453f81e1285
Fagor <oleg-veber@mail.ru>
2024-03-03 15:59:47 +0700

    Change tests

--- problems-2/o-veber/problem-1.py
+++ problems-2/o-veber/problem-1.py
@@ -22,6 +22,18 @@ class TestPythagore(unittest.TestCase):
     def test_two_case(self):
         self.assertEqual([(0, 0, 0), (0, 1, 1), (0, 2, 2), (1, 0, 1), (2, 0, 2)], get_pythagore_triples(2))
 
+    def test_five_case(self):
+        self.assertEqual(
+            [(0, 0, 0), (0, 1, 1), (0, 2, 2), (0, 3, 3), (0, 4, 4), (0, 5, 5), (1, 0, 1), (2, 0, 2), (3, 0, 3),
+             (3, 4, 5), (4, 0, 4), (4, 3, 5), (5, 0, 5)], get_pythagore_triples(5))
+
+    def test_ten_case(self):
+        self.assertEqual(
+            [(0, 0, 0), (0, 1, 1), (0, 2, 2), (0, 3, 3), (0, 4, 4), (0, 5, 5), (0, 6, 6), (0, 7, 7), (0, 8, 8),
+             (0, 9, 9), (0, 10, 10), (1, 0, 1), (2, 0, 2), (3, 0, 3), (3, 4, 5), (4, 0, 4), (4, 3, 5), (5, 0, 5),
+             (6, 0, 6), (6, 8, 10), (7, 0, 7), (8, 0, 8), (8, 6, 10), (9, 0, 9), (10, 0, 10)],
+            get_pythagore_triples(10))
+
 
 if __name__ == "__main__":
     unittest.main()
