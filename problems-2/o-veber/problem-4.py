f88c87c50baa3d2a7b933fcb0f7ad12ff2af99d7
Fagor <oleg-veber@mail.ru>
2024-03-03 16:44:05 +0700

    Fix

--- problems-2/o-veber/problem-4.py
+++ problems-2/o-veber/problem-4.py
@@ -1,13 +1,20 @@
 print("Enter sequence to search for")
 sequence = input()
-with open("pi.txt") as file:
-    file_content = file.read()
-    print(f'Found {file_content.count(sequence)} results')
-    positions = ['0']
+with open("pi.txt", 'r') as file:
+    buffer = ''
+    line = file.readline()
+    while line != '':
+        line = line.replace('\n', '')
+        buffer += line
+        line = file.readline()
+    character_found_count = buffer.count(sequence)
+    print(f'Found {character_found_count} results')
+    positions = ['-1']
     for i in range(1, 6):
-        found_position = file_content.find(sequence, int(positions[i - 1]) + 1)
+        found_position = buffer.find(sequence, int(positions[i - 1]) + 1)
         if found_position == -1:
             break
         positions.append(str(found_position))
     if len(positions) > 1:
-        print(f'Positions: {" ".join(positions[1:6])} ...')
+        many_dots_or_nothing = '...' if character_found_count > 5 else ''
+        print(f'Positions: {" ".join(positions[1:6])}{many_dots_or_nothing}')
