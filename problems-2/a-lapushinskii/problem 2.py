3ccba7a5f85b8ccc82c9615e13f04f35272aff89
chiguaua <91546000+chiguaua@users.noreply.github.com>
2024-03-04 11:35:20 +0700

    stderr error

--- problems-2/a-lapushinskii/problem 2.py
+++ problems-2/a-lapushinskii/problem 2.py
@@ -1,4 +1,5 @@
 import unittest
+import sys
 
 def revers_dict(input_dict):
     output_dict = {}
@@ -10,8 +11,7 @@ def revers_dict(input_dict):
             else:
                 output_dict[latin] = [english]
                 
-    sorted_dict = {key: output_dict[key] for key in sorted(output_dict)}
-    return sorted_dict
+    return output_dict
 
 def read_dict_file(file_path):
     try:
@@ -22,23 +22,24 @@ def read_dict_file(file_path):
                 input_dict[english] = latin_translations.split(', ')
         return input_dict
     except Exception as e:
-        print("Ошибка при открытии файла: ", e)
+        print("Ошибка при открытии файла: ", e, file=sys.stderr)
         return {}
     
 def print_dict_to_file(some_dict, filename):
+    sorted_dict = dict(sorted(some_dict.items()))
     try:
         with open(filename, 'w') as file:
-            for english, latin_words in some_dict.items():
+            for english, latin_words in sorted_dict.items():
                 file.write(f"{english} - ")
                 for latin in latin_words[:-1]:
                     file.write(f"{latin}, ")
                 if latin_words: 
                     file.write(f"{latin_words[-1]}\n")
     except Exception as e:
-        print("Ошибка при сохранении результата: ", e)
+        print("Ошибка при сохранении результата: ", e, file=sys.stderr)
         return {}
    
-latin_english_dict = revers_dict(read_dict_file('input_task_2.txt'))
+latin_english_dict = revers_dict(read_dict_file('inpt_task_2.txt'))
 print_dict_to_file(latin_english_dict, 'output_task_2.txt')
 
 ###
