5523d597994b753916c859c5d94efc53c8ed034a
NikitaTrTr <tatnv01@gmail.com>
2024-03-04 13:26:00 +0700

    Add unit tests

--- problems-1/n-tatarinov/problem1.py
+++ problems-1/n-tatarinov/problem1.py
@@ -1,3 +1,5 @@
+import unittest
+
 def cum_sum(input):
     N = len(input)
     res = [0]*(len(input)+1)
@@ -5,7 +7,18 @@ def cum_sum(input):
         res[i] = res[i-1] + input[i-1]
     return res
 
-assert cum_sum([]) == [0]
-assert cum_sum([10]) == [0, 10]
-assert cum_sum([1,2,3,4]) == [0, 1, 3, 6, 10]
-assert cum_sum([1, -1, 1, -1]) == [0, 1, 0, 1, 0]
+class TestCumSumFunction(unittest.TestCase):
+    def test_empty_input(self):
+        self.assertEqual(cum_sum([]), [0])
+
+    def test_single_element(self):
+        self.assertEqual(cum_sum([10]), [0,10])
+
+    def test_simple_input(self):
+        self.assertEqual(cum_sum([1,2,3,4]), [0,1,3,6,10])
+
+    def test_negative_input(self):
+        self.assertEqual(cum_sum([1, -1, 1, -1]), [0, 1, 0, 1, 0])
+
+if __name__ == '__main__':
+    unittest.main()
