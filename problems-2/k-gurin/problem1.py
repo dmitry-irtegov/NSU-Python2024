9096b8d576eefef2f6555d5647c05d09cdb4f483
Your Name <you@example.com>
2024-03-04 19:47:59 +0700

    tests added

--- problems-2/k-gurin/problem1.py
+++ problems-2/k-gurin/problem1.py
@@ -1,10 +1,24 @@
 # problems-2/assignment-1
+import unittest
+
+
 def pythagorean_threes(n):
     return [(x, y, z) for x in range(n)
-            for y in range(n)
-            for z in range(n)
+            for y in range(x, n)
+            for z in range(y, n)
             if x ** 2 + y ** 2 == z ** 2]
 
 
-print(pythagorean_threes(6))
+class PythagoreanThrees(unittest.TestCase):
+    def test_init(self):
+        self.assertEqual([(0, 0, 0), (0, 1, 1), (0, 2, 2)], pythagorean_threes(3))
 
+    def test_empty_list(self):
+        self.assertEqual([], pythagorean_threes(0))
+
+    def test_one_element(self):
+        self.assertEqual([(0, 0, 0)], pythagorean_threes(1))
+
+
+if __name__ == '__main__':
+    unittest.main()
