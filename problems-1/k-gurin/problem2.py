53a71ca2a8009a3e53e815f3d98e9b4a8940b2e8
Your Name <you@example.com>
2024-03-04 15:20:42 +0700

    test added

--- problems-1/k-gurin/problem2.py
+++ problems-1/k-gurin/problem2.py
@@ -1,12 +1,29 @@
 # problems-1/assignment-2
-#! /usr/bin/python
+import unittest
+
+
 def cut_numbers(arr, a, b):
     for i in range(len(arr)):
         if arr[i] < a:
             arr[i] = a
-        if arr[i] > b:
+        elif arr[i] > b:
             arr[i] = b
     return arr
 
 
-print(cut_numbers([1, 2, 3, 13, 20, 30, 50, 60, 70], 12, 34))
+class TestForCutFunction(unittest.TestCase):
+    def test_with_positive_numbers(self):
+        self.assertEqual([2, 3, 5, 6, 6], cut_numbers([1, 3, 5, 7, 9], 2, 6))
+
+    def test_with_empty_list(self):
+        self.assertEqual([], cut_numbers([], 2, 6))
+
+    def test_with_swap_borders(self):
+        self.assertEqual([6, 6, 6, 2, 2], cut_numbers([1, 3, 5, 7, 9], 6, 2))
+
+    def test_with_mixed_list(self):
+        self.assertEqual([1, -1, 6, 7, -1, 8, 8], cut_numbers([1, -3, 6, 7, -10, 9, 50], -1, 8))
+
+
+if __name__ == '__main__':
+    unittest.main()
