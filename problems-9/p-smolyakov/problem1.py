3a6703ff70f21e12f6a56ff008eb5c197f2c584c
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2024-03-04 13:55:38 +0700

    make members private

--- problems-9/p-smolyakov/problem1.py
+++ problems-9/p-smolyakov/problem1.py
@@ -5,17 +5,17 @@ class Timer:
     def __init__(self, block_name=None):
         if block_name is not None and not isinstance(block_name, str):
             raise TypeError('block_name can be only None or str type')
-        self.block_name = block_name
+        self.__block_name = block_name
 
     def __enter__(self):
-        self.start = time.perf_counter()
+        self.__start = time.perf_counter()
 
     def __exit__(self, exception_type, exception_value, traceback):
-        elapsed = time.perf_counter() - self.start
-        if self.block_name is None:
+        elapsed = time.perf_counter() - self.__start
+        if self.__block_name is None:
             print(f'Time elapsed: {elapsed} s')
         else:
-            print(f'Time elapsed in block \'{self.block_name}\': {elapsed} s')
+            print(f'Time elapsed in block \'{self.__block_name}\': {elapsed} s')
 
 if __name__ == '__main__':
     with Timer():
