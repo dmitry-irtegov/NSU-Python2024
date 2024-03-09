9e849db7789d1c813a2fe45380e05a4254427a62
vhlebnikov <v.khlebnikov@g.nsu.ru>
2024-03-10 02:09:00 +0700

    Commit to check correctness

--- problems-1/v-khlebnikov/problem4.py
+++ problems-1/v-khlebnikov/problem4.py
@@ -5,7 +5,7 @@ def print_lyrics():
             print(num.title(), " bottle", "s" if num != "one" else "", " hanging on the wall,", sep="")
         print("And if" if num != "one" else "If that", "one green bottle should accidentally fall,")
         print("There’ll be ", numbers[index + 1], " green bottle",
-              "s" if numbers[index+1] != "one" else "", " hanging on the wall.", sep="")
+              "s" if numbers[index + 1] != "one" else "", " hanging on the wall.", sep="")
         if num == "one":
             break
 
