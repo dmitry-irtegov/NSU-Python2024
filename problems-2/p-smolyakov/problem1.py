b7a10b7ab85225346a9db6fd871fa6b7609a2308
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2024-03-10 18:33:00 +0700

    improved error handling

--- problems-2/p-smolyakov/problem1.py
+++ problems-2/p-smolyakov/problem1.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 import sys
+import time
 
 if __name__ == "__main__":
     while True:
         try:   
             user_text = input('Type some integer value: ')
         except EOFError:
-            print('~ An integer still hasn\'t been typed, but no more input is coming. Exiting.')
+            print('\nEnd of input file. Exiting.', file=sys.stderr)
+            sys.exit(1)
+        except KeyboardInterrupt:
+            print('\nSIGINT received. Exiting.', file=sys.stderr)
+            sys.exit(1)
+        except BaseException as e:
+            print(f'\nMet {repr(e)} while reading from stdin. Exiting.', file=sys.stderr)
             sys.exit(1)
 
         try:
             num = int(user_text)
         except ValueError:
-            print(f'{user_text} is not an integer value! Try again.')
+            print(f'\n{user_text} is not an integer value! Try again.', file=sys.stderr)
         else:
-            print(f'You typed {num}. Congratulations!')
+            print(f'\nYou typed {num}. Congratulations!')
             sys.exit(0)
