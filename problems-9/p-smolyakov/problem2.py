2e259e041ceb8dac70d4861e692ce4ee6eb612a8
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2024-03-10 17:15:12 +0700

    style fixes

--- problems-9/p-smolyakov/problem2.py
+++ problems-9/p-smolyakov/problem2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import unittest
 
 class _ReadOnlyDict:
     def __init__(self):
@@ -23,7 +24,7 @@ class _StorageTransaction(_ReadOnlyDict):
     def __delitem__(self, key):
         try:
             del self._dict[key]
-        except KeyError as e:
+        except KeyError:
             raise KeyError(f'Key {key} doesn\'t exist in current snapshot') from None
 
 
@@ -47,14 +48,13 @@ class Storage(_ReadOnlyDict):
 
 
     def edit(self):
-        if self._ongoing_transaction == False:
+        if not self._ongoing_transaction:
             self._ongoing_transaction = True
             return _StorageTransaction(self)
         else:
             raise RuntimeError('some transaction is already in progress')
 
 
-import unittest
 class StorageTransactionsTests(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -77,8 +77,8 @@ class StorageTransactionsTests(unittest.TestCase):
         self.assertEqual(self.__storage[False], '0, actually')
 
         with self.assertRaises(KeyError):
-            tmp = self.__storage['not exist']
-
+            _ = self.__storage['not exist']
+            
 
     def test_single_transaction_with_redefs(self):
         with self.__storage.edit() as se:
@@ -98,7 +98,7 @@ class StorageTransactionsTests(unittest.TestCase):
         self.assertEqual(self.__storage[2], True)
 
         with self.assertRaises(KeyError):
-            tmp = self.__storage[1]
+            _ = self.__storage[1]
 
 
     def test_single_transaction_unsuccessful_deletion(self):
@@ -110,14 +110,14 @@ class StorageTransactionsTests(unittest.TestCase):
 
     def test_user_exception(self):
         with self.assertRaises(NotImplementedError):
-            with self.__storage.edit() as se:
+            with self.__storage.edit() as _:
                 raise NotImplementedError            
 
 
     def test_nested_transaction_failure(self):
         with self.assertRaises(RuntimeError):
-            with self.__storage.edit() as se:
-                with self.__storage.edit() as se2:
+            with self.__storage.edit() as _:
+                with self.__storage.edit() as _:
                     pass
 
 
@@ -137,7 +137,8 @@ class StorageTransactionsTests(unittest.TestCase):
 
         self.assertEqual(self.__storage['one'], 1)
         with self.assertRaises(KeyError):
-            tmp = self.__storage['two']
+            _ = self.__storage['two']
+            
 
 
     def test_regular_multi_transaction(self):
@@ -146,7 +147,7 @@ class StorageTransactionsTests(unittest.TestCase):
 
         self.assertEqual(self.__storage['one'], 1)
         with self.assertRaises(KeyError):
-            tmp = self.__storage['two']
+            _ = self.__storage['two']
 
         with self.__storage.edit() as se:
             se['two'] = 2
@@ -180,7 +181,7 @@ class StorageTransactionsTests(unittest.TestCase):
 
         self.assertEqual(self.__storage['one'], 1)
         with self.assertRaises(KeyError):
-            tmp = self.__storage['two']
+            _ = self.__storage['two']
 
 
 if __name__ == '__main__':
