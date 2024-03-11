aa7cbc11b607f1f8e57a6d240fd3fc72b7aa8ef6
Your Name <you@example.com>
2024-03-11 11:35:24 +0700

    no zero triplets & more tests

--- problems-2/k-gurin/problem1.py
+++ problems-2/k-gurin/problem1.py
@@ -2,8 +2,8 @@
 import unittest
 
 
-def pythagorean_threes(n):
-    return [(x, y, z) for x in range(n)
+def pythagorean_threes(n: int):
+    return [(x, y, z) for x in range(1, n)
             for y in range(x, n)
             for z in range(y, n)
             if x ** 2 + y ** 2 == z ** 2]
@@ -11,13 +11,20 @@ def pythagorean_threes(n):
 
 class PythagoreanThrees(unittest.TestCase):
     def test_init(self):
-        self.assertEqual([(0, 0, 0), (0, 1, 1), (0, 2, 2)], pythagorean_threes(3))
+        self.assertEqual([(3, 4, 5), (5, 12, 13), (6, 8, 10)], pythagorean_threes(15))
 
-    def test_empty_list(self):
+    def test_zero(self):
         self.assertEqual([], pythagorean_threes(0))
 
-    def test_one_element(self):
-        self.assertEqual([(0, 0, 0)], pythagorean_threes(1))
+    def test_negative_number(self):
+        self.assertEqual([], pythagorean_threes(-3))
+
+    def test_pythagorean_triplets(self):
+        triplets = pythagorean_threes(100)
+        for triplet in triplets:
+            a, b, c = triplet
+            self.assertTrue(a ** 2 + b ** 2 == c ** 2)
+        self.assertEqual(len(triplets), len(set(triplets)))
 
 
 if __name__ == '__main__':
