568c4c442695c954f08ef2a9449ef8f46dcf238e
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2024-03-11 16:36:46 +0700

    added print errors

--- problems-2/a.tretyakov/problem-3/problem-3.py
+++ problems-2/a.tretyakov/problem-3/problem-3.py
@@ -1,39 +1,47 @@
-import os
-import sys
-from os.path import isfile, join
+from argparse import ArgumentParser
 from os import listdir, stat
+from os.path import join
+from sys import stderr
 
 
-def get_file_size(file_path):
-    return stat(file_path).st_size
+def get_file_size(path):
+    try:
+        return stat(path).st_size
+    except PermissionError as e:
+        print(f"Cannot retrieve information about file {path}", e, file=stderr)
 
 
 def list_files_by_size(directory):
-    files = [file for file in listdir(directory) if isfile(join(directory, file))]
-    files.sort(key=lambda x: (-get_file_size(join(directory, x)), x))
-    return files
+    directory_files = get_files(directory)
+    retrieved_files = [inner_file for inner_file in directory_files]
+    retrieved_files.sort(key=lambda x: (-get_file_size(join(directory, x)), x))
+    return retrieved_files
 
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python problem-3.py <directory>")
-        return
-
-    directory = sys.argv[1]
-
-    if not os.access(directory, os.F_OK):
-        raise Exception("Directory doesn't exist")
-    elif not os.access(directory, os.R_OK):
-        raise Exception("Directory doesn't readable")
-    elif not os.access(directory, os.X_OK):
-        raise Exception("Directory doesn't executable")
-
-    files = list_files_by_size(directory)
-    for file in files:
-        file_path = join(directory, file)
-        size = get_file_size(file_path)
-        print(f"{file}: {size} bytes")
+def get_files(path):
+    try:
+        return listdir(path)
+    except NotADirectoryError:
+        print(f"Cannot open directory {path}. This is file, not the directory", file=stderr)
+    except FileNotFoundError:
+        print(f"Cannot open directory {path}. Directory not found", file=stderr)
+    except PermissionError:
+        print(f"Cannot open directory {path}. Permission denied", file=stderr)
+    return {}
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        parser = ArgumentParser()
+        parser.add_argument("dir", help="Lists file size in directory. Note: gets ABSOLUTE path of the directory")
+        dir_path = parser.parse_args().dir
+
+        files = list_files_by_size(dir_path)
+
+        for file in files:
+            file_path = join(dir_path, file)
+            size = get_file_size(file_path)
+            print(f"{file}: {size} bytes")
+
+    except BaseException as base_exception:
+        print("Unhandled exception", base_exception, file=stderr)
