38ea8e908501fa09af709606026c2ea9e5b70800
Тимур Ильиных <55120919+m1Myp@users.noreply.github.com>
2024-03-12 13:38:17 +0700

    Update problem1.py

--- problems-6/t-ilinykh/problem1.py
+++ problems-6/t-ilinykh/problem1.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 def shuffle_letters(word):
     letters = list(word)
@@ -18,11 +19,11 @@ def transform_text(text, transformation):
     return ' '.join(transformed_words)
 
 def main():
+    sys.stdout.reconfigure(encoding='utf-8')
     text = input("Введите текст: ")
     try:
         random_result = transform_text(text, shuffle_letters)
         sorted_result = transform_text(text, sort_letters)
-
         print("Исходный текст:")
         print(text)
         print("Случайная перестановка букв:")
