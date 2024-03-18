642ac74748cfa3eb17ebcc5ee52af9a4debab085
Inikta <iniktus@mail.ru>
2024-03-18 17:33:55 +0700

    fixed grammatic issues + made exception text better

--- problems-1/n-batenko/problems-1_n-batenko_task-2.py
+++ problems-1/n-batenko/problems-1_n-batenko_task-2.py
@@ -3,7 +3,7 @@ import unittest
 def substitution_function(seq, a, b):
     result = [0 for i in range(len(seq))]
     if a > b:
-        raise Exception('Lower bound is higher than Higher bound.')
+        raise Exception('Out of bounds: substitution_function: Lower bound is greater than higher bound.')
     for i in range(len(seq)):
         num = seq[i]
         temp = num
@@ -14,7 +14,7 @@ def substitution_function(seq, a, b):
         result[i] = temp
     return result
 
-class SubstiotutionTests(unittest.TestCase):
+class SubstitutionTests(unittest.TestCase):
     def test_equals_dif(self):
         print(substitution_function([0, 0, 0], 1, 5))
         self.assertEqual(substitution_function([0, 0, 0], 1, 5), [1, 1, 1])
