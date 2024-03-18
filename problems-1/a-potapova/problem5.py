5294fdd75486176c4f91f780a2b6bbd656e581e4
coffe <a.potapova8@g.nsu.ru>
2024-03-18 12:13:03 +0700

    fix divisor range

--- problems-1/a-potapova/problem5.py
+++ problems-1/a-potapova/problem5.py
@@ -1,19 +1,20 @@
+import math
 import unittest
 
 
 def into_prime_numbers(number: int) -> list:
     result = []
     current_number = number
-    divisor = 2
     power = 0
-    while current_number != 1:
+    for divisor in range(2, int(math.sqrt(number))+1):
         while current_number % divisor == 0:
             current_number = current_number // divisor
             power += 1
         if power > 0:
             result.append([divisor, power])
             power = 0
-        divisor += 1
+    if current_number != 1:
+        result.append([current_number, 1])
     return result
 
 
@@ -22,6 +23,7 @@ class TestIntoPrimeNumbers(unittest.TestCase):
         self.assertEqual(into_prime_numbers(6), [[2, 1], [3, 1]])
         self.assertEqual(into_prime_numbers(9), [[3, 2]])
         self.assertEqual(into_prime_numbers(21), [[3, 1], [7, 1]])
+        self.assertEqual(into_prime_numbers(31), [[31, 1]])
 
 
 if __name__ == '__main__':
