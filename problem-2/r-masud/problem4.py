674ca6b52533bb38af150d2fe40f9793a04f7762
benmasud <rofikulbenmasud@gmail.com>
2024-03-19 11:50:12 +0700

    fix: problem with pi file - p2t4

--- problem-2/r-masud/problem4.py
+++ problem-2/r-masud/problem4.py
@@ -1,27 +1,40 @@
-def find_sequence(sequence, content):
-    positions = []
-    start_index = 0
-    while True:
-        index = content.find(sequence, start_index)
-        if index == -1:
+import unittest
+
+def find_substring(num):
+    string_occurrences = []
+    found_occurrence = 0
+
+    with open("pi.txt", "r") as f:
+        pi = f.read().replace('\n', '')[2:]
+
+
+    while found_occurrence < len(pi):
+        found_occurrence = pi.find(num, found_occurrence)
+        if found_occurrence == -1:
             break
-        positions.append(index)
-        start_index = index + 1
-    return positions
+        string_occurrences.append(found_occurrence)
+        found_occurrence += 1
 
-def main():
-    with open('pi.txt', 'r') as file:
-        pi_content = file.read()
+    return len(string_occurrences), string_occurrences[:5]
 
-    while True:
-        sequence = input("Enter sequence to search for.\n")
-        positions = find_sequence(sequence, pi_content)
-        num_occurrences = len(positions)
-        print(f"Found {num_occurrences} results.")
-        print("Positions:", " ".join(map(str, positions[:5])))
+class TestFindSubstring(unittest.TestCase):
 
-        if num_occurrences == 0:
-            break
+    def test_first_example_seq(self):
+        expected = (4185, [1923, 2937, 2975, 3891, 6547])
+        self.assertTupleEqual(expected, find_substring("123"))
+
+    def test_second_example_seq(self):
+        expected = (424, [0, 6954, 29135, 45233, 79686])
+        self.assertTupleEqual(expected, find_substring("1415"))
+
+    def test_single_digit(self):
+        expected = (420192, [4, 11, 13, 29, 37])
+        self.assertTupleEqual(expected, find_substring("9"))
+
+    def test_sequence_at_end_of_pi(self):
+        expected = (475, [761, 762, 763, 17987, 19436])
+        self.assertTupleEqual(expected, find_substring("9999"))
+
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
