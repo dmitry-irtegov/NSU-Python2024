e1db75e6c07906b8654c6d7a5e552077eb4f5b84
chiguaua <91546000+chiguaua@users.noreply.github.com>
2024-03-24 20:38:59 +0700

    замена на генератор.

--- problems-1/a-lapushinskii/problem3.py
+++ problems-1/a-lapushinskii/problem3.py
@@ -1,16 +1,16 @@
 import unittest
 
 def collatz_row(n):
-    row = [n]
-    while (n > 1):
-        if(n%2 == 1):
-            n = 3*n + 1
-            row.append(n)
-        n = n//2
-        row.append(n)
-    return row
+    yield n
+    while n > 1:
+        if n % 2 == 1:
+            n = 3 * n + 1
+        else:
+            n = n // 2
+        yield n
 
-def print_row_string(row):
+def print_row_string(generator):
+    row = list(generator)
     for i in row[:-1]:
         print(str(i), end = " -> ")
     if row:
@@ -23,11 +23,11 @@ print_row_string(collatz_row(n))
     
 class TestCollatzRow(unittest.TestCase):
     def test_examples(self):
-        self.assertEqual(collatz_row(3), [3, 10, 5, 16, 8, 4, 2, 1])
+        self.assertEqual(list(collatz_row(3)), [3, 10, 5, 16, 8, 4, 2, 1])
     def test(self):
-        self.assertEqual(collatz_row(4), [4, 2, 1])
+        self.assertEqual(list(collatz_row(4)), [4, 2, 1])
     def test_minus(self):
-        self.assertEqual(collatz_row(-1), [-1])
+        self.assertEqual(list(collatz_row(-1)), [-1])
 
 if __name__ == '__main__':
     unittest.main()
