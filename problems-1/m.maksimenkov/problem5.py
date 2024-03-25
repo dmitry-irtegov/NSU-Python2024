531a50e1550d04530c264fa7f8fcf0074b9048b3
Maxim <112863178+grilld9@users.noreply.github.com>
2024-03-25 14:56:27 +0700

    O(N)

--- problems-1/m.maksimenkov/problem5.py
+++ problems-1/m.maksimenkov/problem5.py
@@ -8,27 +8,18 @@ def prime_divisors(num):
         return result
     elif num < 0:
         num = abs(num)
-    idx = 0
-    divisor = 2
-    while num != 1:
-        if is_simple(divisor):
-            if num % divisor == 0:
-                degree = 1
-                result.append([divisor, degree])
+    for divisor in range(2, round(math.sqrt(num)) + 1):
+        degree = 0
+        if num % divisor == 0:
+            while num % divisor == 0:
                 num //= divisor
-                while num % divisor == 0:
-                    degree += 1
-                    result[idx][1] = degree
-                    num //= divisor
-                idx += 1
-        divisor += 1
+                degree += 1
+            result.append([divisor, degree])
+    if num != 1:
+        result.append([num, 1])
     return result
-def is_simple(num):
-    sqrt_rounded = round(math.sqrt(num))
-    for i in range(2, sqrt_rounded + 1):
-        if num % i == 0:
-            return False
-    return True
+
+
 class PrimeDivisorsTest(unittest.TestCase):
 
     def test(self):
@@ -38,6 +29,10 @@ class PrimeDivisorsTest(unittest.TestCase):
         self.assertEqual(prime_divisors(123), [[3, 1], [41, 1]])
         self.assertEqual(prime_divisors(562), [[2, 1], [281, 1]])
         self.assertEqual(prime_divisors(12), [[2, 2], [3, 1]])
+
+    def test_performance(self):
+        prime_divisors(9310971384583411)
+
 
 if '__main__' == __name__:
     unittest.main()
