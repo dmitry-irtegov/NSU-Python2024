80ca5c92f0a9eed0969fa82c408fd869293bee1c
benmasud <rofikulbenmasud@gmail.com>
2024-03-25 17:08:07 +0700

    fix: sorted alphabet and improvement of the code and testing

--- problem-2/r-masud/problem2.py
+++ problem-2/r-masud/problem2.py
@@ -1,13 +1,30 @@
 import unittest
 
+def latin_sort_key(latin_word):
+    return latin_word.lower()
+
+
 def create_latin_english_dictionary(english_latin_dict):
     latin_english_dict = {}
 
-    for english_word, latin_translations in english_latin_dict.items():
-        for latin_word in latin_translations:
-            latin_english_dict.setdefault(latin_word, []).append(english_word)
 
-    return latin_english_dict
+    for english_word, latin_words in english_latin_dict.items():
+        for latin_word in latin_words:
+            if latin_word in latin_english_dict:
+                latin_english_dict[latin_word].add(english_word)
+            else:
+                latin_english_dict[latin_word] = {english_word}
+
+    # for sorting the keys in the Latin-English dictionary alphabetically, ignoring case
+    sorted_keys = sorted(latin_english_dict.keys(), key=lambda x: x.lower())
+    sorted_latin_english_dict = {key: sorted(latin_english_dict[key]) for key in sorted_keys}
+
+    return sorted_latin_english_dict
+
+
+
+
+
 
 class TestCreateLatinEnglishDictionary(unittest.TestCase):
     def test_basic_conversion(self):
@@ -27,6 +44,9 @@ class TestCreateLatinEnglishDictionary(unittest.TestCase):
         self.assertEqual(latin_english_dict['popum'], ['fruit'])
         self.assertEqual(latin_english_dict['multa'], ['punishment'])
 
+        # checking if new dictionary elements are sorted
+        self.assertEqual(list(latin_english_dict.keys()), ['baca', 'bacca', 'malum', 'multa', 'pomum', 'popula', 'popum'])
+
     def test_empty_input(self):
         empty_dict = {}
         self.assertEqual(create_latin_english_dictionary(empty_dict), {})
@@ -41,7 +61,12 @@ class TestCreateLatinEnglishDictionary(unittest.TestCase):
 
         latin_english_dict = create_latin_english_dictionary(english_latin_dict)
 
-        self.assertEqual(latin_english_dict['malum'], ['apple', 'punishment', 'orange'])
+        self.assertEqual(latin_english_dict['malum'], ['apple', 'orange', 'punishment'])
+
+        # checking if new dictionary elements are sorted
+        expected_keys = ['baca', 'bacca', 'malum', 'multa', 'pomum', 'popula', 'popum']
+        self.assertEqual(list(latin_english_dict.keys()), expected_keys)
+
 
 if __name__ == '__main__':
     unittest.main()
