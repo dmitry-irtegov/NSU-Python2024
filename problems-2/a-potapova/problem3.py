b22b10f7237e5167873700cd79da38b82e87252b
coffe <a.potapova8@g.nsu.ru>
2024-03-30 15:58:22 +0700

    fix catch while getting size

--- problems-2/a-potapova/problem3.py
+++ problems-2/a-potapova/problem3.py
@@ -14,7 +14,11 @@ def get_files(directory_path: str):
     filenames = os.listdir(directory_path)
     directory_files = [None] * len(filenames)
     for i, filename in enumerate(filenames):
-        size = os.stat(directory_path + '/' + filename).st_size
+        size = -1
+        try:
+            size = os.stat(directory_path + '/' + filename).st_size
+        except OSError:
+            sys.stderr.write(f"Error while getting file size: {directory_path + '/' + filename}\n")
         directory_files[i] = (filename, size)
     directory_files.sort(key=lambda x: (-x[1], x[0]))
     return directory_files
@@ -25,10 +29,11 @@ if __name__ == '__main__':
     try:
         files = get_files(args.path)
     except Exception as e:
-        sys.stderr.write(str(e))
+        sys.stderr.write(f"Error while {str(e)}")
         exit(1)
     print(f"current directory: {args.path}")
     print(f"{'NAME':<50}  {'SIZE (in bytes)':<4}")
     for file in files:
-        print(f"{file[0]:<50}  {file[1]:<4}")
-
+        filename, size = file
+        size = str(size) if size != -1 else "None"
+        print(f"{filename:<50}  {size:<4}")
