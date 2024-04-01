76441fcbce675698e5b8d801f988f36e5ef946b9
Fagorym <oleg-veber@mail.ru>
2024-04-01 10:59:54 +0700

    Some changes

--- problems-3/o-veber/problem-3.py
+++ problems-3/o-veber/problem-3.py
@@ -3,41 +3,46 @@ from unittest import TestCase, main
 
 class Vector:
     def __init__(self, *components):
-        self.components = components
-        print(components)
+        self._components = components
         self.len = len(components)
 
     def __add__(self, other):
-        if other.len != self.len:
-            raise ValueError("Vectors must be same length for \'add\' operation")
-        return Vector(*(x + y for x, y in zip(self.components, other.components)))
+        if isinstance(other, Vector):
+            if other.len != self.len:
+                raise ValueError("Vectors must be same length for \'add\' operation")
+            return Vector(*(s + o for s, o in zip(self._components, other._components)))
+        else:
+            raise TypeError("Unsupported operation")
 
     def __sub__(self, other):
-        if other.len != self.len:
-            raise ValueError("Vectors must be same length for \'sub\' operation")
-        return Vector(*(x - y for x, y in zip(self.components, other.components)))
+        if isinstance(other, Vector):
+            if other.len != self.len:
+                raise ValueError("Vectors must be same length for \'sub\' operation")
+            return Vector(*(s - o for s, o in zip(self._components, other._components)))
+        else:
+            raise TypeError("Unsupported operation")
 
     def __mul__(self, other):
         if isinstance(other, Vector):
-            return sum(a * b for a, b in zip(self.components, other.components))
-        elif isinstance(other, (int, float)):
-            return Vector(*(a * other for a in self.components))
+            return sum(s * o for s, o in zip(self._components, other._components))
+        elif isinstance(other, (int, float, complex)):
+            return Vector(*(s * other for s in self._components))
         else:
             raise TypeError("Unsupported operation")
 
     def __eq__(self, other):
-        return self.components == other.components
+        if isinstance(other, Vector):
+            return self._components == other._components
+        return False
 
     def __len__(self):
         return self.len
 
     def __getitem__(self, index):
-        if index > self.len:
-            raise IndexError("Index must be less than vector size")
-        return self.components[index]
+        return self._components[index]
 
     def __str__(self):
-        return f"Vector({', '.join(map(str, self.components))})"
+        return f"Vector({', '.join(map(str, self._components))})"
 
 
 class TestVector(TestCase):
@@ -46,7 +51,12 @@ class TestVector(TestCase):
         second = Vector(4, 5, 6)
         self.assertEqual(Vector(5, 7, 9), first + second)
 
-    def test_adding_raises_exception(self):
+    def test_adding_raises_type_error(self):
+        first = Vector(1, 2, 3)
+        second = 2
+        self.assertRaises(TypeError, lambda: first + second)
+
+    def test_adding_raises_value_error(self):
         first = Vector(1)
         second = Vector(1, 2)
         self.assertRaises(ValueError, lambda: first + second)
@@ -56,7 +66,12 @@ class TestVector(TestCase):
         second = Vector(4, 5, 6)
         self.assertEqual(Vector(3, 3, 3), second - first)
 
-    def test_subtracting_raises_exception(self):
+    def test_subtracting_raises_type_error(self):
+        first = Vector(1, 2, 3)
+        second = 2
+        self.assertRaises(TypeError, lambda: first - second)
+
+    def test_subtracting_raises_value_error(self):
         first = Vector(1)
         second = Vector(1, 2)
         self.assertRaises(ValueError, lambda: first - second)
@@ -82,6 +97,20 @@ class TestVector(TestCase):
     def test_getitem(self):
         first = Vector(1, 2, 3, 4, 5, 6, 7)
         self.assertEqual(3, first[2])
+
+    def test_equals(self):
+        first = Vector(1, 2, 3)
+        second = Vector(1, 2, 3)
+        self.assertEqual(first, second)
+
+    def test_not_equals(self):
+        first = Vector(1, 2, 3)
+        second = Vector(1, 2, 4)
+        self.assertNotEqual(first, second)
+
+    def test_not_equals_with_different_type(self):
+        first = Vector(1, 2, 3)
+        self.assertNotEqual(first, 2)
 
     def test_getitem_negative(self):
         first = Vector(1, 2, 3, 4, 5, 6, 7)
