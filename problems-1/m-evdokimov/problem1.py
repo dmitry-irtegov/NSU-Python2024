678490c3d4cdf2677f1a0d4587194f47753845dd
Mikhail <evdokimm05@gmail.com>
2024-04-01 12:01:34 +0700

    delete split

--- problems-1/m-evdokimov/problem1.py
+++ problems-1/m-evdokimov/problem1.py
@@ -2,31 +2,29 @@ import unittest
 
 def cumulativeSum(inputList):
 
-    inputList = inputList.split()
-
     length = len(inputList)
     resultList = [None] * (length + 1)
     resultList[0] = 0
 
     for index, number in enumerate(inputList):
-        resultList[index+1] = resultList[index] + int(number)
+        resultList[index+1] = resultList[index] + number
     
     return resultList
 
 class TestCumulativeSum(unittest.TestCase):
   
     def test_123(self):
-        self.assertEqual(cumulativeSum("1 2 3"), [0, 1, 3, 6])
+        self.assertEqual(cumulativeSum([1, 2, 3]), [0, 1, 3, 6])
     
     def test_1(self):
-        self.assertEqual(cumulativeSum("1"), [0, 1])
+        self.assertEqual(cumulativeSum([1]), [0, 1])
         
     def test_below_zero(self):
-        self.assertEqual(cumulativeSum("-1 -2 -3"), [0, -1, -3, -6])
+        self.assertEqual(cumulativeSum([-1, -2, -3]), [0, -1, -3, -6])
         
     def test_exeption(self):
-        with self.assertRaises(ValueError):
-            cumulativeSum("a b c")
+        with self.assertRaises(TypeError):
+            cumulativeSum(['a', 'b', 'c'])
   
 if __name__ == "__main__":
   unittest.main()
