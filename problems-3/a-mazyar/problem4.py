c1c6f8e1fc3ae179c93e62c176aaed759c1e08c4
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-04-01 14:59:34 +0700

    Implement linear algorithm

--- problems-3/a-mazyar/problem4.py
+++ problems-3/a-mazyar/problem4.py
@@ -1,14 +1,40 @@
 from typing import Self
 import unittest
 
+class n_ary_digit():
+    _n: int
+    _val: int
+
+    def __init__(self, n: int):
+        self._n = n
+        self._val = 0
+
+    def value(self) -> int:
+        return self._val
+
+    def inc(self) -> bool:
+        if self._val == self._n - 1:
+            self._val = 0
+            return True
+        self._val += 1
+        return False
+
+def increment_n_ary_list(num: list[n_ary_digit]) -> list[n_ary_digit]:
+    for digit in num:
+       overflown = digit.inc()
+       if not overflown:
+           return num
+       
+    return num
+
 class CartesianProductElement():
     _set: tuple
     _n: int
-    _ids: list[int]
+    _ids: list[n_ary_digit]
     _current_item_id: int
     _next_item_id: int
 
-    def __init__(self, init_set: tuple, n: int, state_ids: list[int] = [None]) -> None:
+    def __init__(self, init_set: tuple, n: int):
         self._set = init_set
         self._n = n
         self._first = self._set[0]
@@ -16,39 +42,21 @@ class CartesianProductElement():
         self._current_item_id = 0
         self._next_item_id = 1
 
-        if state_ids != None:
-            self._ids = state_ids
-        else:
-            self._ids = [0] * n
+        self._ids = [n_ary_digit(len(self._set)) for _ in range(n)]
 
-    def next_subset(self) -> Self:
-        next_ids = self._ids.copy()
-        next_ids[self._current_item_id] += 1
-        if next_ids[self._current_item_id] == self._n - 1:
-            self._current_item_id = self._next_item_id
+    def next_subset(self):
+        # state is m-ary number of length N - to get next elem we just increment it
+        next_ids = increment_n_ary_list(self._ids)
 
-        next_item = self._set[self._next_item_id]
-        next_state[self._current_item_id] = next_item
-        next_state = tuple(next_state)
-
-        if self._next_item_id == self._n - 1:
-            self.
-        try:
-            # search from the end
-            ni, cur_elem = next((self._n - i - 1, elem) for i, elem in enumerate(self._state[::-1]) if elem != self._last)
-        except StopIteration:   # this was the last element in the product
-            return CartesianProductElement(self._set, self._n)
-        
-        next_elem = self._next_elem(cur_elem)
-        new_state = tuple([elem if i < ni else next_elem if i == ni else self._first for i, elem in enumerate(self._state)])
-
-        return CartesianProductElement(self._set, self._n, new_state)
+        next_subset = CartesianProductElement(self._set, self._n)
+        next_subset._ids = next_ids
+        return next_subset
     
     def get_state(self) -> tuple:
-        return self._state
+        return tuple([self._set[i.value()] for i in self._ids])
 
     def __str__(self):
-        return self._state.__str__()
+        return self.get_state().__str__()
 
 
 class TestCartesianProduct(unittest.TestCase):
@@ -59,16 +67,16 @@ class TestCartesianProduct(unittest.TestCase):
             cp = cp.next_subset()
             test_cps += (cp.get_state(), )
 
-        self.assertEqual(test_cps, ((1,1),(1,"a"),("a",1),("a","a")))
+        self.assertCountEqual(test_cps, ((1,1),(1,"a"),("a",1),("a","a")))
 
         test_cps += (cp.next_subset().get_state(), )
-        self.assertEqual(test_cps, ((1,1),(1,"a"),("a",1),("a","a"),(1,1)))
+        self.assertCountEqual(test_cps, ((1,1),(1,"a"),("a",1),("a","a"),(1,1)))
 
     def test_single(self):
         cp = CartesianProductElement((0, ), 5)
         test_cps = (cp.get_state(), )
         test_cps += (cp.next_subset().get_state(), )
-        self.assertEqual(test_cps, ((0,0,0,0,0),(0,0,0,0,0)))
+        self.assertCountEqual(test_cps, ((0,0,0,0,0),(0,0,0,0,0)))
 
     def test_4bit_nums(self):
         cp = CartesianProductElement((0,1), 4)
@@ -77,7 +85,7 @@ class TestCartesianProduct(unittest.TestCase):
             cp = cp.next_subset()
             test_cps += (cp.get_state(), )
             
-        self.assertEqual(test_cps, (
+        self.assertCountEqual(test_cps, (
             (0, 0, 0, 0),
             (0, 0, 0, 1),
             (0, 0, 1, 0),
