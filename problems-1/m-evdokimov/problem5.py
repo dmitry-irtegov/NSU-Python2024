ca5882146c829887981d00e5992bc7aac0a91527
Mikhail <evdokimm05@gmail.com>
2024-04-06 13:19:49 +0700

    list instead of dictionary

--- problems-1/m-evdokimov/problem5.py
+++ problems-1/m-evdokimov/problem5.py
@@ -3,35 +3,39 @@ import unittest
 def factorization(n):
 
     i = 2
-    resList = {}
+    resDict = {}
     
     while i * i <= n:
         while n % i == 0:
-            if i in resList :
-                resList[i] = resList[i] + 1
+            if i in resDict :
+                resDict[i] = resDict[i] + 1
             else:
-                resList[i] = 1
+                resDict[i] = 1
             n = n // i
         i = i + 1
         
     if n > 1:
-        if n in resList :
-            resList[n] = resList[n] + 1
+        if n in resDict :
+            resDict[n] = resDict[n] + 1
         else:
-            resList[n] = 1
+            resDict[n] = 1
+            
+    resList = []
+    for key in resDict:
+        resList.append([key, resDict[key]])
             
     return resList
 
 class TestFactorization(unittest.TestCase):
   
     def test_2(self):
-        self.assertEqual(factorization(2), {2: 1})
+        self.assertEqual(factorization(2), [[2, 1]])
     
     def test_12(self):
-        self.assertEqual(factorization(12), {2: 2, 3: 1})
+        self.assertEqual(factorization(12), [[2, 2], [3, 1]])
         
     def test_109(self):
-        self.assertEqual(factorization(109), {109: 1})
+        self.assertEqual(factorization(109), [[109, 1]])
         
     def test_exeption(self):
         with self.assertRaises(TypeError):
