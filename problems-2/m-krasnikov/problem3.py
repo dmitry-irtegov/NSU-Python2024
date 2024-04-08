f69abb33aa816e768bc92e7bc510ed1db2455f5f
miqqra <mikhail.krasnikov.04@gmail.com>
2024-04-08 15:24:47 +0700

    Problem-2-3

--- problems-2/m-krasnikov/problem3.py
+++ problems-2/m-krasnikov/problem3.py
@@ -6,7 +6,7 @@ def sort_files_by_size(directory_path):
     files = []
 
     for filename in get_filenames(directory_path):
-        file_path = join_path(directory_path, filename)
+        file_path = os.path.join(directory_path, filename)
         if os.path.isfile(file_path):
             file_size = get_file_size(file_path)
             files.append((filename, file_size))
@@ -21,51 +21,40 @@ def get_filenames(directory_path):
     try:
         return os.listdir(directory_path)
     except FileNotFoundError:
-        print("Directory does not exist: '%s'" % directory_path)
+        sys.stderr.write("Directory does not exist: '%s'" % directory_path)
     except PermissionError:
-        print("Permission denied: '%s'" % directory_path)
+        sys.stderr.write("Permission denied: '%s'" % directory_path)
     except OSError:
-        print("Path error: '%s'" % directory_path)
+        sys.stderr.write("Path error: '%s'" % directory_path)
 
 
 def get_file_size(file_path):
     try:
         return os.path.getsize(file_path)
-    except FileNotFoundError:
-        print("File does not exist: '%s'" % directory_path)
-    except PermissionError:
-        print("Permission denied: '%s'" % directory_path)
     except OSError:
-        print("Path '%s' does not exists or is inaccessible" % file_path)
-
-
-def join_path(directory_path, file_path):
-    try:
-        return os.path.join(directory_path, file_path)
-    except OSError:
-        print("Path error: '%s', '%s'" % directory_path, file_path)
+        sys.stderr.write("Error while getting size of file '%s'" % file_path)
 
 
 def is_file(file_path):
     try:
         return os.path.isfile(file_path)
     except FileNotFoundError:
-        print("File does not exist: '%s'" % file_path)
+        sys.stderr.write("File does not exist: '%s'" % file_path)
     except PermissionError:
-        print("Permission denied: '%s'" % file_path)
+        sys.stderr.write("Permission denied: '%s'" % file_path)
     except OSError:
-        print("Path '%s' does not exists or is inaccessible" % file_path)
+        sys.stderr.write("Path '%s' does not exists or is inaccessible" % file_path)
 
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
-        print("Usage: python program.py <directory_path>")
+        sys.stderr.write("Usage: python program.py <directory_path>")
         sys.exit(1)
 
     directory_path = sys.argv[1]
     if not os.path.isdir(directory_path):
-        print("Error: Directory does not exist.")
+        sys.stderr.write("Error: Directory does not exist.")
         sys.exit(1)
 
     sort_files_by_size(directory_path)
