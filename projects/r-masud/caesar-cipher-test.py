6ef7d3022899a0fe36dd1aea9aa991461ece9ddb
benmasud <rofikulbenmasud@gmail.com>
2024-04-08 17:16:23 +0700

    fix: problem with the git testing

--- projects/r-masud/caesar-cipher-test.py
+++ projects/r-masud/caesar-cipher-test.py
@@ -1,11 +1,12 @@
+import unittest
+
 def caesar_decrypt(text, shift, language):
     if language == 'en':
         return decrypt_english(text, shift)
     elif language == 'ru':
         return decrypt_russian(text, shift)
     else:
-        raise ValueError("Unsupported language")
-
+        return "Unsupported language"
 
 def decrypt_english(msg, shift):
     decrypted_text = ""
@@ -17,28 +18,25 @@ def decrypt_english(msg, shift):
             decrypted_text += char
     return decrypted_text
 
-
 def decrypt_russian(msg, shift):
-    lowercase_alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-    uppercase_alphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
+    llst = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+    ulst = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
     decrypted_text = ""
     for char in msg:
-        if char.lower() in lowercase_alphabet:
-            ind = lowercase_alphabet.index(char.lower())
-            decrypted_ind = (ind - shift) % len(lowercase_alphabet)
-            decrypted_char = uppercase_alphabet[decrypted_ind] if char.isupper() else lowercase_alphabet[decrypted_ind]
+        if char.lower() in llst:
+            ind = llst.index(char.lower())
+            decrypted_ind = (ind - shift) % len(llst)
+            decrypted_char = llst[decrypted_ind].upper() if char.isupper() else llst[decrypted_ind]
             decrypted_text += decrypted_char
-        elif char in uppercase_alphabet:
-            ind = uppercase_alphabet.index(char)
-            decrypted_ind = (ind - shift) % len(uppercase_alphabet)
-            decrypted_char = uppercase_alphabet[decrypted_ind].lower() if char.islower() else uppercase_alphabet[decrypted_ind]
+        elif char in ulst:
+            ind = ulst.index(char)
+            decrypted_ind = (ind - shift) % len(ulst)
+            decrypted_char = ulst[decrypted_ind].lower() if char.islower() else ulst[decrypted_ind]
             decrypted_text += decrypted_char
         else:
             decrypted_text += char
     return decrypted_text
 
-
-import unittest
 
 class TestCaesarDecrypt(unittest.TestCase):
 
@@ -59,8 +57,8 @@ class TestCaesarDecrypt(unittest.TestCase):
     def test_unsupported_language(self):
         # Test unsupported language
         encrypted_text = "Wklv lv d whvw phvvdjh."
-        with self.assertRaises(ValueError):
-            caesar_decrypt(encrypted_text, 3, 'fr')
+        result = caesar_decrypt(encrypted_text, 3, 'fr')
+        self.assertEqual(result, "Unsupported language")
 
 
 if __name__ == '__main__':
