c4a4058bb1d5c538ac9010a7f36fdd336aeefa85
inikta <iniktus@mail.ru>
2024-04-08 15:35:52 +0700

    allocation and exception raise fix + renamed file

--- problems-2/n-batenko/problem1.py
+++ problems-2/n-batenko/problem1.py
@@ -2,13 +2,12 @@ import unittest
 from math import sqrt
 
 def pif_triple(n : int):
-    result = []
-
     if (n <= 0):
         raise Exception("pif_triple: n must be more than zero.")
     
+    result = []
 
-    result = [((x, y), z) for x in range(1, int(sqrt(n)) + 1)
+    result = [(x, y, z) for x in range(1, int(sqrt(n)) + 1)
                 for y in range(x, int(sqrt(n)) + 1)
                     for z in range(1, int(sqrt(n)) + 1)
                         if (z**2 == x**2 + y**2) and (z**2 <= n)]
@@ -23,8 +22,8 @@ class TestPifTriple(unittest.TestCase):
     def test_too_little_5(self):
         self.assertEqual(pif_triple(5), [])
 
-    def test_100(self):
-        self.assertEqual(pif_triple(100), [((3, 4), 5), ((6, 8), 10)])
+    def test_z10(self):
+        self.assertEqual(pif_triple(100), [(3, 4, 5), (6, 8, 10)])
 
 if __name__ == '__main__':
     unittest.main()
