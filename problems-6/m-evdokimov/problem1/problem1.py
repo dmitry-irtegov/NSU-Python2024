3eb1d5c099c863e7738efdf867d8197170a6abe5
Mikhail <evdokimm05@gmail.com>
2024-04-08 15:42:13 +0700

    code style

--- problems-6/m-evdokimov/problem1/problem1.py
+++ problems-6/m-evdokimov/problem1/problem1.py
@@ -4,7 +4,7 @@ import argparse
 def random_mix(word, length):
     word_list = list(word)
     letters = word_list[1:length-1]
-    for index, l in enumerate(word[1:length-1]):
+    for index, letter in enumerate(word[1:length-1]):
         rand_index = random.randint(0, len(letters)-1)
         word_list[index+1] = (letters[rand_index])
         letters.pop(rand_index)
@@ -15,13 +15,13 @@ def abc_mix(word, length):
     word_list = list(word)
     letters = word_list[1:length-1]
     letters.sort(key = str.lower)
-    for index, l in enumerate(letters):
+    for index, letter in enumerate(letters):
         word_list[index+1] = l
     return ''.join(word_list)
     
 
 def mixer(string, mode, how_to_print = print):
-    if mode == '' or mode == None:
+    if mode == '' or mode is None:
         mode = 'random'
     if mode != 'abc' and mode != 'random':
         raise ValueError('Wrong mode name')
