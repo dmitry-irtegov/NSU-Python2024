430d667ba1784ac7de68e0e5bc0dfeea26598626
Mikhail <evdokimm05@gmail.com>
2024-04-08 15:37:08 +0700

    pr 6 task 1 init

---
+++ problems-6/m-evdokimov/problem1/problem1.py
@@ -0,0 +1,44 @@
+import random
+import argparse
+
+def random_mix(word, length):
+    word_list = list(word)
+    letters = word_list[1:length-1]
+    for index, l in enumerate(word[1:length-1]):
+        rand_index = random.randint(0, len(letters)-1)
+        word_list[index+1] = (letters[rand_index])
+        letters.pop(rand_index)
+    return ''.join(word_list)
+    
+
+def abc_mix(word, length):
+    word_list = list(word)
+    letters = word_list[1:length-1]
+    letters.sort(key = str.lower)
+    for index, l in enumerate(letters):
+        word_list[index+1] = l
+    return ''.join(word_list)
+    
+
+def mixer(string, mode, how_to_print = print):
+    if mode == '' or mode == None:
+        mode = 'random'
+    if mode != 'abc' and mode != 'random':
+        raise ValueError('Wrong mode name')
+    
+    for word in string.split():
+        length = len(word)
+        if length > 3:
+            if mode == 'random':
+                how_to_print(random_mix(word, length), end = ' ')
+            if mode == 'abc':
+                how_to_print(abc_mix(word, length), end = ' ')
+        else:
+            how_to_print(word, end = ' ')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_string", help="enter your input string here")
+    parser.add_argument("-m", "--mode", help="choose mixer mode ('random' or 'abc')")
+    args = parser.parse_args()
+    mixer(args.input_string, args.mode) 
---
+++ problems-6/m-evdokimov/problem1/test.py
@@ -0,0 +1,28 @@
+import unittest
+import word_mixer
+
+test_output = []
+
+def custom_print(*ins, **args):
+    word = ins[0]
+    test_output.append(word)
+    
+class Test_mixer(unittest.TestCase):
+    
+    def setUp(self):
+        test_output.clear()
+  
+    def test_abc(self):
+        word_mixer.mixer("Hello   world !", 'abc', how_to_print = custom_print)
+        self.assertEqual(test_output, ['Hello', 'wlord', '!'])
+        
+    def test_short_words(self):
+        word_mixer.mixer("Hey how are you ?", 'random', how_to_print = custom_print)
+        self.assertEqual(test_output, ['Hey', 'how', 'are', 'you', '?'])
+        
+    def test_exeption(self):
+        with self.assertRaises(ValueError):
+            word_mixer.mixer("Hello world", 'bobr')
+  
+if __name__ == "__main__":
+  unittest.main()
