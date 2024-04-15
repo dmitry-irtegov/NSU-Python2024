e8c00fb0aea5983ecbb7b541cc3d3199039a6c34
Vlad Lomakin <v.lomakin1@g.nsu.ru>
2024-04-15 09:30:05 +0700

    Добавил тесты и кодировку

--- problems-4/v-lomakin/problem-2/tests.py
+++ problems-4/v-lomakin/problem-2/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from tr import tr
 import unittest
 
@@ -21,14 +22,23 @@ class TestTr (unittest.TestCase):
     def test_equal(self):
         self.assertEqual(tr("ex", "", ""), "ex")
 
+    def test_equal2(self):
+        self.assertEqual(tr("ex", "av", "tg"), "ex")
+
     def test_equalArgs(self):
         self.assertEqual(tr("abcd", "abe", "abe"), "abcd")
 
     def test_deleteNothing(self):
-        self.assertEqual(tr("abcd", "ab", "cd", ""), "cdcd")
+        self.assertEqual(tr("abcd", "ab", "ef", ""), "efcd")
 
     def test_oneLetter(self):
         self.assertEqual(tr("bbbbbbbbbbb", "b", "C"), "CCCCCCCCCCC")
+
+    def test_oneElse(self):
+        self.assertEqual(tr("ab", "abcdef", "ijklmn"), "ij")
+
+    def test_moreTests(self):
+        self.assertEqual(tr("abcdefgij", "aej", "lmn"), "lbcdmfgin")
 
 if __name__ == "__main__":
     unittest.main()
