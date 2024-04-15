9437883d9906a9aa850bab1d9d6a13d2c0608cd4
Мельников Андрей <a.melnikov4@g.nsu.ru>
2024-04-15 18:31:16 +0700

    Fix sth to satisfy mypy

--- problems-3/a-melnikov/problem1/main.py
+++ problems-3/a-melnikov/problem1/main.py
@@ -1,4 +1,4 @@
-from typing import Self, TypeVar, Generic
+from typing import Self, TypeVar, Generic, Sequence
 
 T = TypeVar("T")
 
@@ -9,7 +9,7 @@ class Table(Generic[T]):
     columns: int
     table: list[list[T | None]]
 
-    def __init__(self, rows: int, columns: int, table: list[list[T | None]] | None = None):
+    def __init__(self, rows: int, columns: int, table: Sequence[Sequence[T | None]] | None = None):
         self.rows = rows
         self.columns = columns
         self.table: list[list[T | None]] = [[None] * columns for _ in range(rows)]
@@ -32,7 +32,7 @@ class Table(Generic[T]):
         for i, row in enumerate(row_indices):
             for j in range(self.columns):
                 table[i][j] = self.table[row][j]
-        return Table(rows, self.columns, table)
+        return type(self)(rows, self.columns, table)
 
     def select_columns(self, *column_indices: int) -> Self:
         columns: int = len(column_indices)
@@ -40,7 +40,7 @@ class Table(Generic[T]):
         for i in range(self.rows):
             for j, col in enumerate(column_indices):
                 table[i][j] = self.table[i][col]
-        return Table(self.rows, columns, table)
+        return type(self)(self.rows, columns, table)
 
     def join_rows(self, table: Self) -> Self:
         if table.columns != self.columns:
@@ -56,7 +56,7 @@ class Table(Generic[T]):
             for i in range(table.rows):
                 res_table[self.rows + i][j] = table.table[i][j]
 
-        return Table(rows, columns, res_table)
+        return type(self)(rows, columns, res_table)
 
     def join_columns(self, table: Self) -> Self:
         if table.rows != self.rows:
@@ -72,4 +72,4 @@ class Table(Generic[T]):
             for j in range(table.columns):
                 res_table[i][self.columns + j] = table.table[i][j]
 
-        return Table(rows, columns, res_table)
+        return type(self)(rows, columns, res_table)
