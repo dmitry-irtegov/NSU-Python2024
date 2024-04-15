16a4c8696e38b6cf89403ac73c6ce0a8940b1684
inikta <iniktus@mail.ru>
2024-04-15 16:17:50 +0700

    test fix

--- problems-1/n-batenko/problem4.py
+++ problems-1/n-batenko/problem4.py
@@ -7,7 +7,7 @@ def green_bottles():
     green_bottle = " green bottle"
     comma = ",\n"
     dot = "."
-    hanging_on_the_wall = " hanging on the wall"
+    hanging_on_the_wall = ' hanging on the wall'
     and_if = "And if " 
     green_bottle_should_accidentally_fall = green_bottle + " should accidentally fall"
     threre_ll_be = "There'll be "
@@ -18,7 +18,7 @@ def green_bottles():
             result += numbers[i].title() + green_bottle
             
             if (i != (len(numbers) - 2)):
-                result += "s"
+                result += 's'
                 
             result += hanging_on_the_wall + comma
 
@@ -36,55 +36,55 @@ def green_bottles():
             result += 's'
         result += hanging_on_the_wall + dot
 
-        if (i != len(numbers) - 1):
-            result += "\n"
+        if (i != len(numbers) - 2):
+            result += '\n'
 
     return result
 
 class GreenBottlesTest(unittest.TestCase):
     def test(self):
-        ref = """Ten green bottles hanging on the wall,\n
-Ten green bottles hanging on the wall,\n
-And if one green bottle should accidentally fall,\n
-There'll be nine green bottles hanging on the wall.\n
-Nine green bottles hanging on the wall,\n
-Nine green bottles hanging on the wall,\n
-And if one green bottle should accidentally fall,\n
-There'll be eight green bottles hanging on the wall.\n
-Eight green bottles hanging on the wall,\n
-Eight green bottles hanging on the wall,\n
-And if one green bottle should accidentally fall,\n
-There'll be seven green bottles hanging on the wall.\n
-Seven green bottles hanging on the wall,\n
-Seven green bottles hanging on the wall,\n
-And if one green bottle should accidentally fall,\n
-There'll be six green bottles hanging on the wall.\n
-Six green bottles hanging on the wall,\n
-Six green bottles hanging on the wall,\n
-And if one green bottle should accidentally fall,\n
-There'll be five green bottles hanging on the wall.\n
-Five green bottles hanging on the wall,\n
-Five green bottles hanging on the wall,\n
-And if one green bottle should accidentally fall,\n
-There'll be four green bottles hanging on the wall.\n
-Four green bottles hanging on the wall,\n
-Four green bottles hanging on the wall,\n
-And if one green bottle should accidentally fall,\n
-There'll be three green bottles hanging on the wall.\n
-Three green bottles hanging on the wall,\n
-Three green bottles hanging on the wall,\n
-And if one green bottle should accidentally fall,\n
-There'll be two green bottles hanging on the wall.\n
-Two green bottles hanging on the wall,\n
-Two green bottles hanging on the wall,\n
-And if one green bottle should accidentally fall,\n
-There'll be one green bottle hanging on the wall.\n
-One green bottle hanging on the wall,\n
-One green bottle hanging on the wall,\n
-If that one green bottle should accidentally fall,\n
+        self.maxDiff = None
+        ref = """Ten green bottles hanging on the wall,
+Ten green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be nine green bottles hanging on the wall.
+Nine green bottles hanging on the wall,
+Nine green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be eight green bottles hanging on the wall.
+Eight green bottles hanging on the wall,
+Eight green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be seven green bottles hanging on the wall.
+Seven green bottles hanging on the wall,
+Seven green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be six green bottles hanging on the wall.
+Six green bottles hanging on the wall,
+Six green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be five green bottles hanging on the wall.
+Five green bottles hanging on the wall,
+Five green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be four green bottles hanging on the wall.
+Four green bottles hanging on the wall,
+Four green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be three green bottles hanging on the wall.
+Three green bottles hanging on the wall,
+Three green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be two green bottles hanging on the wall.
+Two green bottles hanging on the wall,
+Two green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be one green bottle hanging on the wall.
+One green bottle hanging on the wall,
+One green bottle hanging on the wall,
+If that one green bottle should accidentally fall,
 There'll be no green bottles hanging on the wall."""
         self.assertEqual(green_bottles(), ref)
 
 if __name__ == "__main__":
     unittest.main()
-print(green_bottles())
