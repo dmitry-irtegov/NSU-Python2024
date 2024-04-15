39caa5a65d35ec8112d36aca493a2548aeb1c609
Dmitriy Krasnov <d.krasnov@protonmail.com>
2024-04-15 11:50:38 +0700

    count only digits after dot

--- problems-2/d-krasnov/problem4_test.py
+++ problems-2/d-krasnov/problem4_test.py
@@ -6,13 +6,13 @@ class TestFindSequence(unittest.TestCase):
     def test_find_sequence_123(self):
         pi_file = 'pi.txt'
         sequence = '123'
-        expected_result = [1949, 2976, 3015, 3942, 6632]
+        expected_result = [1947, 2974, 3013, 3940, 6630]
         self.assertEqual(find_sequence(sequence, pi_file)[:5], expected_result)
 
     def test_find_sequence_999(self):
         pi_file = 'pi.txt'
         sequence = '999'
-        expected_result = [772, 773, 774, 775, 2987]
+        expected_result = [770, 771, 772, 773, 2985]
         self.assertEqual(find_sequence(sequence, pi_file)[:5], expected_result)
 
 if __name__ == '__main__':
