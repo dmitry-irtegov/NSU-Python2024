3d42b28b5d56e50371d2dab2439dde2d85c67f7f
Inikta <iniktus@mail.ru>
2024-04-15 12:57:21 +0700

    fixed algorythm

--- problems-2/n-batenko/problem1.py
+++ problems-2/n-batenko/problem1.py
@@ -5,25 +5,23 @@ def pif_triple(n : int):
     if (n <= 0):
         raise Exception("Exception: In pif_triple input, n must be greater than zero.")
     
-    result = []
+    return [(x, y, z)
+            for x in range(1, n + 1)
+            for y in range(x, n + 1)
+            for z in range(y, n + 1)
+            if (x**2 + y**2 == z**2)]
 
-    result = [(x, y, z) for x in range(1, int(sqrt(n)) + 1)
-                for y in range(x, int(sqrt(n)) + 1)
-                    for z in range(1, int(sqrt(n)) + 1)
-                        if (z**2 == x**2 + y**2) and (z**2 <= n)]
-
-    return result
 
 class TestPifTriple(unittest.TestCase):
 
     def test_too_little_1(self):
         self.assertEqual(pif_triple(1), [])
 
-    def test_too_little_5(self):
-        self.assertEqual(pif_triple(5), [])
+    def test_too_5(self):
+        self.assertEqual(pif_triple(5), [(3, 4, 5)])
 
     def test_z10(self):
-        self.assertEqual(pif_triple(100), [(3, 4, 5), (6, 8, 10)])
+        self.assertEqual(pif_triple(10), [(3, 4, 5), (6, 8, 10)])
     
     def test_exception(self):
         with self.assertRaises(Exception) as context:
@@ -32,3 +30,5 @@ class TestPifTriple(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+print(pif_triple(10))
