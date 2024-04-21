00bb64819a298d947bdd7d02e6e966c4349cd926
Mikhail <evdokimm05@gmail.com>
2024-04-21 19:15:51 +0700

    html parser

--- problems-7/m-evdokimov/problem-1.py
+++ problems-7/m-evdokimov/problem-1.py
@@ -1,61 +1,88 @@
+import argparse
 import requests
 import time
-import argparse
+from html.parser import HTMLParser
 
 def wiki_searcher(start_link):
-    infobox = '<table class="infobox'
-    philLink = '%D0%A4%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D1%8F"'
-    content = requests.get(startLink).text
+    philLink = '/wiki/%D0%A4%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D1%8F'
+    content = requests.get(start_link).text
     cnt = 0
     title_list = []
-    new_link_flag = True
-
-    while True:
-        if new_link_flag:
-            header_index = content.find('<span class="mw-page-title-main">') + 33
-            header_index_end = content[header_index:].find('<') + header_index
-            header = content[header_index:header_index_end]
-            print(str(cnt) + ') ' + header)
-        
-            if header in title_list:
+    work_flag = True
+    
+    while work_flag:
+        parser = MyHTMLParser()
+        try:
+            parser.feed(content)
+        except StopIteration as ex:
+            title, next_link = ex.args
+            print(cnt, title)
+            cnt = cnt + 1
+            if title in title_list:
                 print("Цикл!")
-                break
+                work_flag = False
+            title_list.append(title)
+            if next_link == philLink:
+                print("Философия!")
+                work_flag = False
+            time.sleep(2)
+            content = requests.get('https://ru.wikipedia.org' + next_link).text
+
+class MyHTMLParser(HTMLParser):
+    
+    tag = ''
+    link_flag = False
+    title_flag = False
+    title = ''
+    next_link = ''
+    
+    def handle_starttag(self, tag, attrs):
+        
+        if tag == 'p':
+            self.tag = 'p'
+            self.link_flag = True
             
-            title_list.append(header)
+        if tag == 'h1':
+            self.tag = 'h1'
+            self.title_flag = True
+        
+        if self.link_flag and tag == 'a':
+            link_ok = False
+            for atr, value in attrs:
+                if atr == 'href':
+                    link_ok = True
+                    self.tag = 'link'
+                    self.next_link = value
             
-        box_index = content.find(infobox) 
-        if box_index != -1:
-            box_index_end = content[box_index:].find('</table>')
-            content = content[box_index_end:]
+        if self.title_flag and tag == 'span':
+            self.tag = 'title'
+
+    def handle_endtag(self, tag):
         
-        start = content.find("<p>")
-        end = content.find("</p>") + 1
-        scope = content[start:end]
-        content = content[end:]
+        if tag == 'p':
+            self.link_flag = False
+        if tag == 'h1':
+            self.title_flag = False
+        self.tag = ''
         
-        index = scope.find('<a href="/wiki/')
-        if index == -1:
-            new_link_flag = False
-            print("Нет ссылки в абзаце")
-            continue
-        if scope[index:].find(philLink) != -1:
-            cnt += 1
-            print(str(cnt) + ') ' + "Философия")
-            break
-        index = index + 15
+
+    def handle_data(self, data):
         
-        index_end = scope[index:].find('"') + index
+        if self.tag == 'link':
+            self.close()
         
-        link = scope[index : index_end]
-        cnt += 1
-        time.sleep(2)
-        new_link_flag = True
-        content = requests.get('https://ru.wikipedia.org/wiki/' + link).text
+        if self.tag == 'title':
+            self.title = data
+            
+    def close(self):
         
-#startLink = 'https://ru.wikipedia.org/wiki/%D0%92%D1%81%D0%B5%D0%BC%D0%B8%D1%80%D0%BD%D0%B0%D1%8F_%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F'
-#startLink = 'https://ru.wikipedia.org/wiki/%D0%98%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F_%D1%84%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D0%B8'
+        raise StopIteration(self.title, self.next_link)
+
+# 'https://ru.wikipedia.org/wiki/%D0%9D%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB-%D0%B4%D0%B5%D0%BC%D0%BE%D0%BA%D1%80%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D0%BF%D0%B0%D1%80%D1%82%D0%B8%D1%8F_%D0%A2%D0%B8%D0%B1%D0%B5%D1%82%D0%B0'
+# 'https://ru.wikipedia.org/wiki/%D0%92%D1%81%D0%B5%D0%BC%D0%B8%D1%80%D0%BD%D0%B0%D1%8F_%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F'
+# 'https://ru.wikipedia.org/wiki/%D0%98%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F_%D1%84%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D0%B8'
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("start_link", help="enter your start link here")
     args = parser.parse_args()
-    wiki_searcher(args.start_link) 
+    wiki_searcher(args.start_link)
