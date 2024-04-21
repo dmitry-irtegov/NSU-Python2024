5920cff54fbd46ca1cc9bb16fb74e1f996c96616
Inikta <iniktus@mail.ru>
2024-04-21 17:15:20 +0700

    refactored code and enchanced algorithm speed

--- problems-1/n-batenko/problem5.py
+++ problems-1/n-batenko/problem5.py
@@ -1,40 +1,16 @@
 from math import sqrt
 import unittest
 
-def decompose_to_primes(n):    
+def decompose_to_primes(n : int):    
     num = abs(n)
 
     if num < 2:
-        return [()]
+        return (())
     
     result = []
-    primes = []
-    
-    def is_prime(num):
-        if num in primes:
-            return True
-        
-        k = 1
-        for i in range(2, int(sqrt(num)) + 1, k):
-            if num % i == 0:
-                return False
-
-            if i == 3:
-                k += 1
-        
-        if (num not in primes):
-            primes.append(num)
-
-        return True
-
-    if is_prime(num):
-        return [(num, 1)]
     
     k = 1
-    for factor in range(2, num // 2 + 1, k):
-        if (not is_prime(factor)):
-            continue
-
+    for factor in range(2, int(sqrt(num)) + 1, k):
         power = 0
         while num % factor == 0:
             num //= factor
@@ -48,17 +24,29 @@ def decompose_to_primes(n):
         if factor == 3: 
             k = k + 1
 
-    return result
+    if abs(n) > 1 and result == []:
+        return ((abs(n), 1))
+
+    return tuple(map(lambda t: (t[0], t[1]), result))
 
 class DecomposerTests(unittest.TestCase):
-    def test_36(self):
-        self.assertEqual(decompose_to_primes(36), [(2,2), (3,2)])
-    
-    def test_5(self):
-        self.assertEqual(decompose_to_primes(5), [(5, 1)])
-    
+
     def test_1(self):
-        self.assertEqual(decompose_to_primes(1), [()])
+        self.assertEqual(decompose_to_primes(1), (()))
+
+    def test_5(self):
+        self.assertEqual(decompose_to_primes(5), ((5, 1)))
+
+    def test_36(self):
+        self.assertEqual(decompose_to_primes(36), ((2, 2), (3, 2)))
+    
+    def test_1000(self):
+        self.assertEqual(decompose_to_primes(1000), ((2, 3), (5, 3)))
+
+    def test_1234554321(self):
+        self.assertEqual(decompose_to_primes(1234554321), ((3, 1), (7, 1), (11, 1), (13, 1), (37, 1), (41, 1), (271, 1)))
 
 if __name__ == '__main__':
     unittest.main()
+
+print(decompose_to_primes(36))
