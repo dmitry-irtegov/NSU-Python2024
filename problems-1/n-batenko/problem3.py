92682bb53a4262465975933851b7847feaba1ed5
Inikta <iniktus@mail.ru>
2024-04-21 14:43:25 +0700

    refactored functions, made collatz_conjecture lazy (yield) and added few more tests

--- problems-1/n-batenko/problem3.py
+++ problems-1/n-batenko/problem3.py
@@ -2,53 +2,52 @@ import unittest
 import unittest.mock as mock
 import builtins
 
-def collatz_conjecture(n : int):
-
-    if (n <= 0): 
-        raise Exception("Value Error: Invalid number on input of collatz_conjecture. Try positive integer.")
-
-    seq = [n]
-
-    while n != 1:
+def collatz_conjecture(n):
+    yield n
+    while True:
+        if (n == 1):
+            break
         if n % 2 == 0:
             n //= 2
         else:
             n = 3 * n + 1
-        seq.append(n)
-    
-    return seq
 
-def collatz_input():
+        yield n
+
+def collatz_string():
     inp = input("Enter positive integer number: ")
     if (inp.lstrip('-+').isdigit()):
-        num = int(inp)
+        n = int(inp)
     else:
         raise Exception("Value Error: Not an integer on input.")
-    
-    return collatz_conjecture(num)
 
-def collatz_string(seq):
-    result = ''
-    for i in range(len(seq)):
-        result += str(seq[i])
-        if i != (len(seq) - 1):
-            result += ' -> '
+    if (n <= 0): 
+        raise Exception("Value Error: Invalid number on input. Try positive integer.")
     
+    result = ""
+    for num in list(collatz_conjecture(n)):
+        result += str(num)
+        if (num != 1):
+            result += ' -> '
+
     return result
 
 class CollatzConjectureTests(unittest.TestCase):
     
+    def test_1(self):
+        self.assertEqual(list(collatz_conjecture(1)), [1])
+
+    def test_2(self):
+        self.assertEqual(list(collatz_conjecture(2)), [2, 1])
+
     def test_3(self):
-        print(collatz_string(collatz_conjecture(3)))
-        self.assertEqual(collatz_conjecture(3), [3, 10, 5, 16, 8, 4, 2, 1])
+        self.assertEqual(list(collatz_conjecture(3)), [3, 10, 5, 16, 8, 4, 2, 1])
     
     def test_19(self):
-        print(collatz_string(collatz_conjecture(19)))
-        self.assertEqual(collatz_conjecture(19), [19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
+        self.assertEqual(list(collatz_conjecture(19)), [19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
 
     def test_27(self):
-        print(collatz_string(collatz_conjecture(27)))
-        self.assertEqual(collatz_conjecture(27), [27, 82, 41, 124, 62, 31, 94, 47,
+        self.assertEqual(list(collatz_conjecture(27)), [27, 82, 41, 124, 62, 31, 94, 47,
                                                     142, 71, 214, 107, 322, 161, 484,
                                                       242, 121, 364, 182, 91, 274, 137,
                                                         412, 206, 103, 310, 155, 466, 233,
@@ -66,22 +65,33 @@ class CollatzConjectureTests(unittest.TestCase):
     def test_exception_with_input(self):
         with mock.patch.object(builtins, 'input', lambda _: 'one'):
             with self.assertRaises(Exception) as context:
-                 collatz_input()
+                 collatz_string()
             self.assertTrue("Value Error: Not an integer on input." in str(context.exception))
 
-    def test_is_positive_with_input(self):
+    def test_is_positive_exception(self):
         with mock.patch.object(builtins, 'input', lambda _: '-1'):
             with self.assertRaises(Exception) as context:
-                 collatz_input()
-            self.assertTrue("Value Error: Invalid number on input of collatz_conjecture. Try positive integer." in str(context.exception))
-
-    def test_3_with_input(self):
-        with mock.patch.object(builtins, 'input', lambda _: '3'):
-            self.assertEqual(collatz_input(), [3, 10, 5, 16, 8, 4, 2, 1])
+                 collatz_string()
+            self.assertTrue("Value Error: Invalid number on input. Try positive integer." in str(context.exception))
     
-    def test_collatz_string_with_input(self):
+    def test_collatz_string_1(self):
+        with mock.patch.object(builtins, 'input', lambda _: '1'):
+            self.assertEqual(collatz_string(), "1")
+        
+    def test_collatz_string_2(self):
+        with mock.patch.object(builtins, 'input', lambda _: '2'):
+            self.assertEqual(collatz_string(), "2 -> 1")
+
+    def test_collatz_string_3(self):
         with mock.patch.object(builtins, 'input', lambda _: '3'):
-            self.assertEqual(collatz_string(collatz_input()), "3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1")
+            self.assertEqual(collatz_string(), "3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1")
+
+    def test_collatz_string_19(self):
+        with mock.patch.object(builtins, 'input', lambda _: '19'):
+            self.assertEqual(collatz_string(), "19 -> 58 -> 29 -> 88 -> 44 -> 22 -> 11 -> 34 -> 17 -> 52 -> 26 -> 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1")
+
 
 if __name__ == '__main__':
     unittest.main()
+
+print(collatz_string())
