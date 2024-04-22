b5c6e06ea5dde8ae86127d689a753b7eed68b749
Вадим <v.khlebnikov@g.nsu.ru>
2024-04-22 13:43:58 +0700

    Simplify

--- problems-2/v-khlebnikov/problem-4/problem4.py
+++ problems-2/v-khlebnikov/problem-4/problem4.py
@@ -1,4 +1,4 @@
-import time
+import sys
 import unittest
 
 
@@ -15,29 +15,36 @@ def print_pi_positions(counter, positions):
         print()
 
 
-def find_substring(x, file_path):
+def find_positions_in_pi(x, file_path):
     counter = 0
     answers = [0] * 5
-    with open(file_path, "r") as file:
-        s = "".join(map(lambda y: y.strip(), file.readlines()))
-        index = s.find(x)
-        while index != -1:
-            if counter < 5:
-                answers[counter] = index
-            counter += 1
-            index = s.find(x, index + 1)
+    try:
+        file = open(file_path, "r")
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found", file=sys.stderr)
+        exit(1)
+    else:
+        with file:
+            s = "".join(map(lambda line: line.strip(), file.readlines()))
+            index = s.find(x)
+            while index != -1:
+                if counter < 5:
+                    answers[counter] = index
+                counter += 1
+                index = s.find(x, index + 1)
+
     return counter, answers
 
 
 class TestPiFinder(unittest.TestCase):
 
     def test_first(self):
-        counter, positions = find_substring("123", 'pi.txt')
+        counter, positions = find_positions_in_pi("123", 'pi.txt')
         self.assertEqual(4185, counter)
         self.assertEqual([1923, 2937, 2975, 3891, 6547], positions)
 
     def test_second(self):
-        counter, positions = find_substring("1415", 'pi.txt')
+        counter, positions = find_positions_in_pi("1415", 'pi.txt')
         self.assertEqual(424, counter)
         self.assertEqual([0, 6954, 29135, 45233, 79686], positions)
 
