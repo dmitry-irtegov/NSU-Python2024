928c0cb892c9f35b12c0a504645e91379d2773e4
buljad <i.zhuravskii@g.nsu.ru>
2024-04-22 15:11:49 +0700

    Problems-2-2

---
+++ problems-1/i-zhuravskii/problem2.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+def list_cut(numbers, lower_border, upper_border):
+    return [lower_border if num < lower_border
+            else upper_border if num > upper_border
+    else num for num in numbers]
+
+
+class TestListCut(unittest.TestCase):
+    def test_list_cut_1(self):
+        self.assertEquals(list_cut([1, 2, 3, 4, 5], 1, 5), [1, 2, 3, 4, 5])
+
+    def test_list_cut_2(self):
+        self.assertEquals(list_cut([1, 2, 3, 4, 5], 2, 4), [2, 2, 3, 4, 4])
+
+    def test_list_cut_3(self):
+        self.assertEquals(list_cut([1, 2, 3, 4, 5], 1, 2), [1, 2, 2, 2, 2])
+
+
+if __name__ == "__main__":
+    unittest.main()
