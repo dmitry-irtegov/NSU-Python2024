d0475d3791d78732f0681bb7d4d4fbd298047d0f
NikitaTrTr <tatnv01@gmail.com>
2024-04-22 16:52:24 +0700

    Add different ways to return result

--- problems-2/n-tatarinov/problem2/problem2.py
+++ problems-2/n-tatarinov/problem2/problem2.py
@@ -1,6 +1,7 @@
 import unittest
 
-def to_lat_eng(filename):
+
+def to_lat_eng(filename, as_list=False):
     translations = {}
     with open(filename, 'r') as f:
         for line in f.readlines():
@@ -12,18 +13,20 @@ def to_lat_eng(filename):
                     translations[word] = [eng]
     for key in translations:
         translations[key].sort()
-        translations[key] = ', '.join(translations[key])
-    res = [0] * len(translations)
-    i = 0
-    for key in translations:
-        res[i] = f'{key} - {translations[key]}'
-        i += 1
-    res.sort()
-    return res
+    if as_list:
+        res = [0] * len(translations)
+        i = 0
+        for key in translations:
+            res[i] = f'{key} - {", ".join(translations[key])}'
+            i += 1
+        res.sort()
+        return res
+    return dict(sorted(translations.items()))
+
 
 class TestLatEngDictionary(unittest.TestCase):
     def test_dictionary_from_task(self):
-        lat_eng = to_lat_eng('problems-2/n-tatarinov/problem2/dictionary.txt')
+        lat_eng = to_lat_eng('problems-2/n-tatarinov/problem2/dictionary.txt', as_list=True)
         self.assertEqual(lat_eng[0], 'baca - fruit')
         self.assertEqual(lat_eng[1], 'bacca - fruit')
         self.assertEqual(lat_eng[2], 'malum - apple, punishment')
@@ -31,6 +34,17 @@ class TestLatEngDictionary(unittest.TestCase):
         self.assertEqual(lat_eng[4], 'pomum - apple')
         self.assertEqual(lat_eng[5], 'popula - apple')
         self.assertEqual(lat_eng[6], 'popum - fruit')
+
+    def test_dictionary_raw_output(self):
+        lat_eng = to_lat_eng('problems-2/n-tatarinov/problem2/dictionary.txt', as_list=False)
+        self.assertEqual(lat_eng, {'baca': ['fruit'],
+                                   'bacca': ['fruit'],
+                                   'malum': ['apple', 'punishment'],
+                                   'multa': ['punishment'],
+                                   'pomum': ['apple'],
+                                   'popula': ['apple'],
+                                   'popum': ['fruit']})
+
 
 if __name__ == '__main__':
     unittest.main()
