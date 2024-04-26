bd452415f3a36f769c7d56f5e3079d6815a4fe32
Mikhail <evdokimm05@gmail.com>
2024-04-26 12:42:04 +0700

    fix problem with links in tables

--- problems-7/m-evdokimov/problem-1/problem-1.py
+++ problems-7/m-evdokimov/problem-1/problem-1.py
@@ -26,6 +26,7 @@ def wiki_searcher(start_link):
                 print("Философия!")
                 work_flag = False
             time.sleep(2)
+            #print(next_link)
             content = requests.get('https://ru.wikipedia.org' + next_link).text
 
 class MyHTMLParser(HTMLParser):
@@ -33,6 +34,7 @@ class MyHTMLParser(HTMLParser):
     tag = ''
     link_flag = False
     title_flag = False
+    table_flag = False
     title = ''
     next_link = ''
     
@@ -42,11 +44,14 @@ class MyHTMLParser(HTMLParser):
             self.tag = 'p'
             self.link_flag = True
             
+        if tag == 'table':
+            self.table_flag = True
+            
         if tag == 'h1':
             self.tag = 'h1'
             self.title_flag = True
         
-        if self.link_flag and tag == 'a':
+        if self.link_flag and tag == 'a' and not self.table_flag:
             for atr, value in attrs:
                 if atr == 'href':
                     self.tag = 'link'
@@ -61,6 +66,8 @@ class MyHTMLParser(HTMLParser):
             self.link_flag = False
         if tag == 'h1':
             self.title_flag = False
+        if tag == 'table':
+            self.table_flag = False
         self.tag = ''
         
 
@@ -76,9 +83,8 @@ class MyHTMLParser(HTMLParser):
         
         raise StopIteration(self.title, self.next_link)
 
-# 'https://ru.wikipedia.org/wiki/%D0%9D%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB-%D0%B4%D0%B5%D0%BC%D0%BE%D0%BA%D1%80%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D0%BF%D0%B0%D1%80%D1%82%D0%B8%D1%8F_%D0%A2%D0%B8%D0%B1%D0%B5%D1%82%D0%B0'
-# 'https://ru.wikipedia.org/wiki/%D0%92%D1%81%D0%B5%D0%BC%D0%B8%D1%80%D0%BD%D0%B0%D1%8F_%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F'
-# 'https://ru.wikipedia.org/wiki/%D0%98%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F_%D1%84%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D0%B8'
+#start_link = 'https://ru.wikipedia.org/wiki/%D0%A3%D0%BB%D0%B8%D1%86%D0%B0_%D0%A1%D0%B0%D0%B2%D0%B5%D0%BB%D1%8C%D0%B5%D0%B2%D0%B0_(%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0)'
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("start_link", help="enter your start link here")
