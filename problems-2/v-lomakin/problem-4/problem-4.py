65644bad7046f6e785d6caa91afc1705c95bcfb9
Vlad Lomakin <v.lomakin1@g.nsu.ru>
2024-04-28 14:57:44 +0700

    Проверка принта

--- problems-2/v-lomakin/problem-4/problem-4.py
+++ problems-2/v-lomakin/problem-4/problem-4.py
@@ -1,4 +1,13 @@
 import unittest
+result = ""
+
+def myPrint(x, **kvarg):
+    global result
+    if isinstance(x, str):
+        result = result + x
+
+origPrint = print
+print = myPrint
 
 def findPos (sub):
     with open('pi.txt', 'r') as file:
@@ -8,15 +17,41 @@ def findPos (sub):
     while (x != -1):
         list.append(x - 1)
         x = pi.find(sub, x+1)
+    cnt = len(list)
+    print(f"Found {cnt} results.\n")
+    if cnt < 5 :
+        print(f"Positions: {list}\n")
+    else :
+        newList = []
+        for i in list[:5]:
+            newList.append(i)
+        print(f"Positions: {newList}\n")
+        
     file.close()
     return list
 
 class TestsFindPosition(unittest.TestCase):
     def test_qwerty(self):
-        self.assertEqual(findPos("123456"),[2458885,3735793])   
+        global result
+        findPos("123456")
+        self.assertEqual(result,"Found 2 results.\nPositions: [2458885, 3735793]\n")
+        origPrint(result)
+        result = ""
 
     def test_five(self):
-        self.assertEqual(findPos("10049"),[81181, 81663, 164755, 166002, 227951, 382558, 432784, 544247, 556921, 576840, 587404, 605735, 683581, 801882, 810013, 916813, 965597, 1338969, 1501899, 1659357, 1816026, 1951287, 1979822, 2083050, 2119932, 2134021, 2140467, 2166789, 2297621, 2558072, 2571475, 2581813, 2636173, 2668633, 2745051, 2850004, 2854857, 2867894, 2887618, 3059735, 3100890, 3457720, 3506380, 3547206, 3634950, 3819063, 3918388, 4074393, 4191306])
+        global result
+        findPos("10049")
+        self.assertEqual(result,"Found 49 results.\nPositions: [81181, 81663, 164755, 166002, 227951]\n")
+        origPrint(result)
+        result = ""
+
+    def test_one(self):
+        global result
+        findPos("1")
+        self.assertEqual(result,"Found 419139 results.\nPositions: [1, 3, 37, 40, 49]\n")
+        origPrint(result)
+        result = ""
 
 if __name__ == '__main__':
     unittest.main()
+    origPrint(result)
