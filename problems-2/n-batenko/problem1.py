5c9498c89f1e975daf3c625259b4ac0216e7160a
Inikta <iniktus@mail.ru>
2024-04-28 14:56:03 +0700

    made algorithm quadratic + added universal test

--- problems-2/n-batenko/problem1.py
+++ problems-2/n-batenko/problem1.py
@@ -1,14 +1,14 @@
 import unittest
+from math import sqrt
 
 def pif_triple(n : int):
     if (n <= 0):
         raise Exception("Exception: In pif_triple input, n must be greater than zero.")
     
-    return [(x, y, z)
-            for x in range(1, n + 1)
-            for y in range(x, n + 1)
-            for z in range(y, n + 1)
-            if (x**2 + y**2 == z**2)]
+    return [(x, y, int(sqrt(x**2 + y**2)))
+            for x in range(1, n)
+            for y in range(1, n)
+            if sqrt(x**2 + y**2).is_integer() and x**2 + y**2 <= n**2]
 
 
 class TestPifTriple(unittest.TestCase):
@@ -17,11 +17,15 @@ class TestPifTriple(unittest.TestCase):
         self.assertEqual(pif_triple(1), [])
 
     def test_too_5(self):
-        self.assertEqual(pif_triple(5), [(3, 4, 5)])
+        self.assertEqual(pif_triple(5), [(3, 4, 5), (4, 3, 5)])
 
     def test_z10(self):
-        self.assertEqual(pif_triple(10), [(3, 4, 5), (6, 8, 10)])
+        self.assertEqual(pif_triple(10), [(3, 4, 5), (4, 3, 5), (6, 8, 10), (8, 6, 10)])
     
+    def test_any(self, num = 1000):
+        for (x, y, z) in pif_triple(num):
+            self.assertTrue(x**2 + y**2 == z**2 & z**2 <= num ** 2)
+
     def test_exception(self):
         with self.assertRaises(Exception) as context:
             pif_triple(-100)
