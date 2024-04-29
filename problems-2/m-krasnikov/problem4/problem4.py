d24b1564c72e19d86a38496e80b600bc95211b3d
Mikhail Krasnikov <75527785+miqqra@users.noreply.github.com>
2024-04-29 16:57:19 +0700

    Problem-2-4 (#187)
    * Problem-2-4
    
    * Problem-2-4
    
    * Problem-2-4
    
    * Problem-2-4
    
    * Problem-2-4

---
+++ problems-2/m-krasnikov/problem4/problem4.py
@@ -0,0 +1,141 @@
+import gc
+import os
+import time
+import unittest
+
+import psutil
+
+
+def find_substring_chunks(num):
+    occurrences = []
+    last_sliced_index = 0
+    chunks = ""
+    with open("pi.txt") as f:
+        while True:
+            chunk = f.read(10000)
+            if not chunk:
+                break
+
+            chunk = chunk.replace('\n', '')
+
+            chunks += chunk
+
+            occurrence_index = chunks.find(num)
+            while occurrence_index != -1:
+                occurrences.append(last_sliced_index + occurrence_index)
+                occurrence_index += 1
+                last_sliced_index += occurrence_index
+                chunks = chunks[occurrence_index:]
+                occurrence_index = chunks.find(num)
+
+    return len(occurrences), occurrences[:5]
+
+
+def find_substring_lines(num):
+    occurrences = []
+    occurrence_index = 0
+    pi = ""
+
+    with open("pi.txt") as f:
+        for line in f.readlines():
+            pi += line.replace('\n', '')
+
+        occurrence_index = pi.find(num, occurrence_index)
+        while occurrence_index != -1:
+            occurrences.append(occurrence_index)
+            occurrence_index += 1
+            occurrence_index = pi.find(num, occurrence_index)
+
+        return len(occurrences), occurrences[:5]
+
+
+class TestFindSubstring(unittest.TestCase):
+
+    def tearDown(self):
+        gc.collect()
+
+    def test_digit_chunks(self):
+        time_start = time.process_time()
+        self.assertTupleEqual((419907, [14, 30, 40, 48, 57]), find_substring_chunks("7"))
+        print(
+            f"test_digit_chunks: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+    def test_digit_lines(self):
+        time_start = time.process_time()
+        self.assertTupleEqual((419907, [14, 30, 40, 48, 57]), find_substring_lines("7"))
+        print(
+            f"test_digit_lines: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+    def test_small_num_chunks(self):
+        time_start = time.process_time()
+        self.assertTupleEqual((4185, [1925, 2939, 2977, 3893, 6549]), find_substring_chunks("123"))
+        print(
+            f"test_small_num_chunks: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+    def test_small_num_lines(self):
+        time_start = time.process_time()
+        self.assertTupleEqual((4185, [1925, 2939, 2977, 3893, 6549]), find_substring_lines("123"))
+        print(
+            f"test_small_num_lines: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+    def test_big_num_chunks(self):
+        time_start = time.process_time()
+        self.assertTupleEqual((1, [2587993]), find_substring_chunks("9925449022087269459849405"))
+        print(
+            f"test_big_num_chunks: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+    def test_big_num_lines(self):
+        time_start = time.process_time()
+        self.assertTupleEqual((1, [2587993]), find_substring_lines("9925449022087269459849405"))
+        print(
+            f"test_big_num_lines: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+    def test_large_num_chunks(self):
+        time_start = time.process_time()
+        self.assertTupleEqual(
+            (1, [3509922]),
+            find_substring_chunks(
+                "1298089764749344925275601751397259626844842134839054624385860528916070194755434157027205684181170287"))
+        print(
+            f"test_large_num_chunks: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+    def test_large_num_lines(self):
+        time_start = time.process_time()
+        self.assertTupleEqual(
+            (1, [3509922]),
+            find_substring_lines(
+                "1298089764749344925275601751397259626844842134839054624385860528916070194755434157027205684181170287"))
+        print(
+            f"test_large_num_lines: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+    def test_large_num_lines_no(self):
+        time_start = time.process_time()
+        self.assertTupleEqual(
+            (0, []),
+            find_substring_lines(
+                "1298089764749344925275601751397259626844843134839054624385860528916070194755434157027205684181170287"))
+        print(
+            f"test_large_num_lines_no: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+    def test_large_num_chunks_no(self):
+        time_start = time.process_time()
+        self.assertTupleEqual(
+            (0, []),
+            find_substring_chunks(
+                "1298089764749344925275601751397259626844843134839054624385860528916070194755434157027205684181170287"))
+        print(
+            f"test_large_num_chunks_no: time - {time.process_time() - time_start}, "
+            f"memory - {psutil.Process(os.getpid()).memory_info().rss}")
+
+
+if __name__ == "__main__":
+    unittest.main()
