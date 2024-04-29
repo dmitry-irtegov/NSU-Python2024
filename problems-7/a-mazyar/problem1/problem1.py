c83ed34cd1981696d99b8d35fd48d19fc3eccdbe
Мазяр Алексей Николаевич <94759449+tobaffoon@users.noreply.github.com>
2024-04-29 16:41:34 +0700

    Problem7-1 (#159)
    * Add problem7-1
    
    * Add error and parentheses handling
    
    * Fix dead-end articles handling
    
    * Add more fine-grained exception handling
    
    * Add general except for requests
    
    * Fix link and doubled printing
    
    * Remove extra except statements
    
    * Add requirements.txt
    
    * Fix () in links and lookup in <p> in <table>
    
    * Fix regex string

---
+++ problems-7/a-mazyar/problem1/problem1.py
@@ -0,0 +1,79 @@
+import requests
+import regex
+from bs4 import BeautifulSoup
+from time import sleep
+from sys import stderr, exit
+
+WIKI_LINK_BASE = "https://en.wikipedia.org/wiki/"
+
+def is_wiki_normal(wiki_link: str) -> bool:
+    '''
+    Checks if link is a blue link to some wiki page and isn't a note
+    '''
+    if 'role' in wiki_link.attrs and wiki_link['role'] == "note":
+        return False
+    
+    return 'href' in wiki_link.attrs and wiki_link['href'].find("/wiki/") != -1
+
+def remove_parentheses(text: str) -> str:
+    return regex.sub(r"(?![^<]*<\/a>)\(([^()]|(?R))*\)", "", text)
+
+def seek_philosopy(article: str) -> str:
+    result = (article, )
+    print(article)
+    while article != "Philosophy":
+        sleep(0.5)
+        url = ''.join((WIKI_LINK_BASE, article))
+        try:
+            response = requests.get(
+                url=url,
+            )
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as errh:
+            print(f"HTTP Error occured when sending request to {url}. Shutting down.\n\nError description:\n{errh.args[0]}", file=stderr)
+            exit(-1)
+        except requests.RequestException:
+            print(f"Troubles sending request to {url}. Shutting down")
+            exit(-1)
+        
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        link_found: bool = False
+
+        # Remove table elements
+        main_content = soup.find(id="mw-content-text")
+        if(main_content.table is not None):
+            main_content.table.decompose()
+
+        for paragraph in main_content.find_all("p"):
+            # we create artificial paragraph where all parantheses and their contents were removed
+            psoup = BeautifulSoup()
+            p = psoup.new_tag('p')
+            p.append(BeautifulSoup(remove_parentheses(str(paragraph)), 'html.parser'))
+            psoup.append(p)
+
+            links = list(filter(is_wiki_normal, psoup.find_all("a")))
+            if len(links) != 0:
+                article = links[0]['href'][6:] # remove 'wiki/' part
+                link_found = True
+                break
+
+        if not link_found:
+            # index.php?title=MOS:FIRST&redirect=no
+            print(f"Philosophy wasn't reached: No more links to follow from {url}", file=stderr)
+            exit(-1)
+        
+        if article in result:
+            # Tax
+            print(f"Philosophy wasn't reached: Loop occured with {article} article", file=stderr)
+            exit(-1)
+        
+        result += (article, )
+        print(article)
+
+    print(" -> ".join(result))
+    return result
+
+if __name__ == "__main__":
+    start_article = input()
+    seek_philosopy(start_article)
