4588bdb5f6910cc4535f51ced218c524c3a3a347
Your Name <you@example.com>
2024-05-06 15:13:40 +0700

    Always return list. Split [] into separate code

--- problems-2/k-gurin/problem3.py
+++ problems-2/k-gurin/problem3.py
@@ -11,19 +11,24 @@ def parse() -> str:
 
 
 def get_files(path: str) -> list:
-    try:
-        files = [
-            (file, os.stat(os.path.join(path, file)).st_size)
-            for file in os.listdir(path)
-            if os.path.isfile(os.path.join(path, file))
-        ]
-        return sorted(files, key=lambda item: (-item[1], item[0]))
-    except OSError:
-        sys.stderr.write("Error while reading files")
+    files = []
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        if os.path.isfile(file_path):
+            try:
+                file_size = os.stat(file_path).st_size
+                files.append((file, file_size))
+            except OSError as e:
+                print(f"Error while getting file info for '{file}': {e}")
+
+    return sorted(files, key=lambda item: (-item[1], item[0]))
 
 
 if __name__ == '__main__':
     path_to_dir = parse()
-    files_list = get_files(path_to_dir)
-    for f in files_list:
-        print(f[0], "\t", f[1])
+    try:
+        files_list = get_files(path_to_dir)
+        for f in files_list:
+            print(f"{f[0]} \t {f[1]}")
+    except Exception as e:
+        sys.stderr.write(f"Error while getting files in \"{path_to_dir}\": {str(e)}\n")
