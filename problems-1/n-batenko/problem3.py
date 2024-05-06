9524ec71a460dfcd238831c76ce64c267fa5ad71
Inikta <iniktus@mail.ru>
2024-05-06 10:07:18 +0700

    made type exception and lazy output

--- problems-1/n-batenko/problem3.py
+++ problems-1/n-batenko/problem3.py
@@ -19,18 +19,16 @@ def collatz_string():
     if (inp.lstrip('-+').isdigit()):
         n = int(inp)
     else:
-        raise Exception("Value Error: Not an integer on input.")
+        raise ValueError("Not an integer on input.")
 
     if (n <= 0): 
-        raise Exception("Value Error: Invalid number on input. Try positive integer.")
+        raise ValueError("Invalid number on input. Try positive integer.")
     
-    result = ""
     for num in collatz_conjecture(n):
-        result += str(num)
+        res = str(num)
         if (num != 1):
-            result += ' -> '
-
-    return result
+            res += ' -> '
+        yield res
 
 class CollatzConjectureTests(unittest.TestCase):
     
@@ -47,48 +45,35 @@ class CollatzConjectureTests(unittest.TestCase):
         self.assertEqual(tuple(collatz_conjecture(19)), (19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1))
 
     def test_27(self):
-        self.assertEqual(tuple(collatz_conjecture(27)), (27, 82, 41, 124, 62, 31, 94, 47,
-                                                    142, 71, 214, 107, 322, 161, 484,
-                                                      242, 121, 364, 182, 91, 274, 137,
-                                                        412, 206, 103, 310, 155, 466, 233,
-                                                          700, 350, 175, 526, 263, 790, 395,
-                                                            1186, 593, 1780, 890, 445, 1336, 668,
-                                                              334, 167, 502, 251, 754, 377, 1132, 566,
-                                                                283, 850, 425, 1276, 638, 319, 958, 479,
-                                                                  1438, 719, 2158, 1079, 3238, 1619, 4858,
-                                                                    2429, 7288, 3644, 1822, 911, 2734, 1367,
-                                                                      4102, 2051, 6154, 3077, 9232, 4616, 2308,
-                                                                        1154, 577, 1732, 866, 433, 1300, 650, 325,
-                                                                          976, 488, 244, 122, 61, 184, 92, 46, 23, 70,
-                                                                            35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1))
+        self.assertEqual(tuple(collatz_conjecture(27)), (27, 82, 41, 124, 62, 31, 94, 47, 142, 71, 214, 107, 322, 161, 484, 242, 121, 364, 182, 91, 274, 137, 412, 206, 103, 310, 155, 466, 233, 700, 350, 175, 526, 263, 790, 395, 1186, 593, 1780, 890, 445, 1336, 668, 334, 167, 502, 251, 754, 377, 1132, 566, 283, 850, 425, 1276, 638, 319, 958, 479, 1438, 719, 2158, 1079, 3238, 1619, 4858, 2429, 7288, 3644, 1822, 911, 2734, 1367, 4102, 2051, 6154, 3077, 9232, 4616, 2308, 1154, 577, 1732, 866, 433, 1300, 650, 325, 976, 488, 244, 122, 61, 184, 92, 46, 23, 70, 35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1))
         
     def test_wrong_input_exception(self):
         with mock.patch.object(builtins, 'input', lambda _: 'one'):
-            with self.assertRaises(Exception) as context:
-                 collatz_string()
-            self.assertTrue("Value Error: Not an integer on input." in str(context.exception))
+            with self.assertRaises(ValueError) as context:
+                "".join(list(collatz_string()))
+            self.assertTrue("Not an integer on input." in str(context.exception))
 
     def test_is_positive_exception(self):
         with mock.patch.object(builtins, 'input', lambda _: '-1'):
-            with self.assertRaises(Exception) as context:
-                 collatz_string()
-            self.assertTrue("Value Error: Invalid number on input. Try positive integer." in str(context.exception))
+            with self.assertRaises(ValueError) as context:
+                "".join(list(collatz_string()))
+            self.assertTrue("Invalid number on input. Try positive integer." in str(context.exception))
     
     def test_collatz_string_1(self):
         with mock.patch.object(builtins, 'input', lambda _: '1'):
-            self.assertEqual(collatz_string(), "1")
+            self.assertEqual("".join(list(collatz_string())), "1")
         
     def test_collatz_string_2(self):
         with mock.patch.object(builtins, 'input', lambda _: '2'):
-            self.assertEqual(collatz_string(), "2 -> 1")
+            self.assertEqual("".join(list(collatz_string())), "2 -> 1")
 
     def test_collatz_string_3(self):
         with mock.patch.object(builtins, 'input', lambda _: '3'):
-            self.assertEqual(collatz_string(), "3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1")
+            self.assertEqual("".join(list(collatz_string())), "3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1")
 
     def test_collatz_string_19(self):
         with mock.patch.object(builtins, 'input', lambda _: '19'):
-            self.assertEqual(collatz_string(), "19 -> 58 -> 29 -> 88 -> 44 -> 22 -> 11 -> 34 -> 17 -> 52 -> 26 -> 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1")
+            self.assertEqual("".join(list(collatz_string())), "19 -> 58 -> 29 -> 88 -> 44 -> 22 -> 11 -> 34 -> 17 -> 52 -> 26 -> 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1")
 
 
 if __name__ == '__main__':
