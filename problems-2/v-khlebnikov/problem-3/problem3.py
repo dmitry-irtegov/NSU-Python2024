e8cd5a25c89cb3e9e1c4c2cc7a2106bce90e0dd9
Вадим <v.khlebnikov@g.nsu.ru>
2024-05-06 11:41:56 +0700

    Erro handling

--- problems-2/v-khlebnikov/problem-3/problem3.py
+++ problems-2/v-khlebnikov/problem-3/problem3.py
@@ -8,50 +8,31 @@ def get_listdir(path):
     try:
         return os.listdir(path)
     except PermissionError as e:
-        raise PermissionError("Cannot get list of files in '" + path + "': permission denied: " + e)
+        e.strerror = "Cannot get list of files in '" + path + "': permission denied: " + e.strerror
+        raise e
     except FileNotFoundError as e:
-        raise FileNotFoundError("Cannot get list of files in '" + path + "': directory wasn't found: " + e)
+        e.strerror = "Cannot get list of files in '" + path + "': directory wasn't found: " + e.strerror
+        raise e
     except NotADirectoryError as e:
-        raise NotADirectoryError("Cannot get list of files in '" + path + "': path is not a directory: " + e)
+        e.strerror = "Cannot get list of files in '" + path + "': path is not a directory: " + e.strerror
+        raise e
     except OSError  as e:
-        raise OSError("Cannot get list of files in '" + path + "': " + e)
+        e.strerror = "Cannot get list of files in '" + path + "': " + e.strerror
+        raise e
 
 
 def get_file_size(name):
     try:
         return os.stat(name).st_size
     except PermissionError as e:
-        raise PermissionError("Cannot access " + name + ": Permission denied: " + e)
+        e.strerror = "Cannot access " + name + ": Permission denied: " + e.strerror
+        raise e
     except FileNotFoundError as e:
-        raise FileNotFoundError("Cannot access " + name + ": File not found: " + e)
+        e.strerror = "Cannot access " + name + ": File not found: " + e.strerror
+        raise e
     except OSError as e:
-        raise OSError("Cannot access " + name + ": " + e)
-
-
-def print_files(path):
-    try:
-        filenames = get_listdir(path)
-    except BaseException as e:
-        print("Trying to get list of files, but:" + e, file=sys.stderr)
-        exit(1)
-
-    files = {}
-    longest_string_size = 0
-
-    for f in filenames:
-        longest_string_size = max(longest_string_size, len(f))
-        file_path = os.path.join(path, f)
-        try:
-            if os.path.isfile(file_path):
-                files[f] = get_file_size(file_path)
-        except BaseException as e:
-            print("Trying to get size of file '" + file_path + "' but: " + e, file=sys.stderr)
-            files[f] = -1
-                    
-    files = sorted(files.items(), key=lambda x: x[1], reverse=True)
-    for f in files:
-        file_size_string = "{:8}".format(humanize.naturalsize(f[1]))
-        print('{:{val}}'.format(f[0], val=longest_string_size) + " size: " + file_size_string)
+        e.strerror = "Cannot access " + name + ":  " + e.strerror
+        raise e
 
 
 def main():
@@ -59,7 +40,29 @@ def main():
     parser.add_argument('path', help='directory path')
     args = parser.parse_args()
 
-    print_files(args.path)
+    try:
+        filenames = get_listdir(args.path)
+    except BaseException as e:
+        print("Trying to get list of files, but: " + e.strerror, file=sys.stderr)
+        exit(1)
+
+    files = {}
+    longest_string_size = 0
+
+    for f in filenames:
+        longest_string_size = max(longest_string_size, len(f))
+        file_path = os.path.join(args.path, f)
+        try:
+            if os.path.isfile(file_path):
+                files[f] = get_file_size(file_path)
+        except BaseException as e:
+            print("Trying to get size of file '" + file_path + "' but: " + e.strerror, file=sys.stderr)
+            files[f] = -1
+                    
+    files = sorted(files.items(), key=lambda x: x[1], reverse=True)
+    for f in files:
+        file_size_string = "{:8}".format(humanize.naturalsize(f[1]))
+        print('{:{val}}'.format(f[0], val=longest_string_size) + " size: " + file_size_string)
 
 
 if __name__ == '__main__':
