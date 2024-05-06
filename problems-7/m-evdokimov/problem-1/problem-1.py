2365384a9d459aaa47c3bdbd656aa327a5e5c706
Mikhail <evdokimm05@gmail.com>
2024-05-06 13:33:45 +0700

    persistent cache

--- problems-7/m-evdokimov/problem-1/problem-1.py
+++ problems-7/m-evdokimov/problem-1/problem-1.py
@@ -1,5 +1,6 @@
 import argparse
 import requests
+import pickle
 import time
 from html.parser import HTMLParser
 
@@ -7,10 +8,14 @@ def wiki_searcher(start_link):
     wiki_index = start_link.find('/wiki/')
     if wiki_index == -1:
         raise ValueError("Your start link is not from Wiki")
-    cache = {}
+    try:
+        with open("cache.pkl", mode="rb") as file:
+            cache = pickle.load(file)
+    except FileNotFoundError:
+        cache = {}
+    links_cache = {}
     philLink = '/wiki/%D0%A4%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D1%8F'
     cnt = 0
-    title_list = []
     next_link = start_link[wiki_index:]
     
     while True:
@@ -18,21 +23,26 @@ def wiki_searcher(start_link):
             content = cache[next_link]
         else:
             content = requests.get('https://ru.wikipedia.org' + next_link).text
+            cache[next_link] = content
         parser = MyHTMLParser()
         try:
             parser.feed(content)
-        except StopIteration as ex:
+        except StopParsing as ex:
+            current_link = next_link
             title, next_link = ex.args
             print(cnt, title)
             cnt = cnt + 1
-            if title in title_list:
+            if next_link in links_cache.keys():
+                print(links_cache[next_link])
                 print("Цикл!")
                 break
-            title_list.append(title)
             if next_link == philLink:
                 print("Философия!")
                 break
+            links_cache[current_link] = title
             time.sleep(2)
+    with open("cache.pkl", mode="wb") as file:
+        pickle.dump(cache, file)
 
 class MyHTMLParser(HTMLParser):
     
@@ -88,7 +98,14 @@ class MyHTMLParser(HTMLParser):
             
     def close(self):
         
-        raise StopIteration(self.title, self.next_link)
+        raise StopParsing(self.title, self.next_link)
+    
+class StopParsing(Exception):
+    
+    def __init__(self, title, next_link):
+        super().__init__(title, next_link)
+        self.strerror = "No errors, just wanna stop parsing"
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
