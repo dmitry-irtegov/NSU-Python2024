03d69434e219c6d4a5cf5476159cba7cca5c9204
Your Name <you@example.com>
2024-05-06 13:11:01 +0700

    Division to root

--- problems-1/k-gurin/problem5.py
+++ problems-1/k-gurin/problem5.py
@@ -1,37 +1,37 @@
 # problems-1/assignment-5
+import math
 import unittest
 
 
-def prime_factors(n):
-    result = []
-    probe = 2
-    i = temp = count = 0
-    while n != 1:
-        if n % probe != 0:
-            probe += 1
-        else:
-            n /= probe
-            if temp != probe:
-                result.append([probe])
-                if count != 0:
-                    result[i - 1].append(count)
-                i += 1
-                count = 0
+def factorize(num):
+    primes = []
+    for d in range(2, int(math.sqrt(num)) + 1):
+        count = 0
+        while num % d == 0:
+            num //= d
             count += 1
-            temp = probe
-    result[i - 1].append(count)
-    return result
+        if count > 0:
+            primes.append([d, count])
+    if num > 1:
+        primes.append([num, 1])
+    return primes
 
 
 class PrimeFactors(unittest.TestCase):
+    def test_zero(self):
+        self.assertEqual(factorize(0), [])
+
+    def test_one(self):
+        self.assertEqual(factorize(1), [])
+
     def test_init(self):
-        self.assertEqual([[2, 2], [3, 1]], prime_factors(12))
+        self.assertEqual(factorize(12), [[2, 2], [3, 1]])
 
     def test_prime(self):
-        self.assertEqual([[3571, 1]], prime_factors(3571))
+        self.assertEqual(factorize(3571), [[3571, 1]])
 
     def test_big_split_number(self):
-        self.assertEqual([[2, 1], [3, 2], [11, 1], [17, 1], [37, 1]], prime_factors(124542))
+        self.assertEqual(factorize(124542), [[2, 1], [3, 2], [11, 1], [17, 1], [37, 1]])
 
 
 if __name__ == '__main__':
