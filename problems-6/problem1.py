40ff6170084d5353c3bdd5c3ff86f9a70854c714
NikFive <NikIYPT@yandex.ru>
2024-05-06 13:54:41 +0700

    konstantinov-problems6-1/add check for punctuations mark

--- problems-6/problem1.py
+++ problems-6/problem1.py
@@ -24,8 +24,11 @@ def process_text(text, word_processor):
     words = text.split()
     processed_words = []
     for word in words:
-        processed_word = word_processor(word)
-        processed_words.append(processed_word)
+        alphanumeric_word = ''.join(char for char in word if char.isalpha())
+        if alphanumeric_word:
+            processed_word = word_processor(alphanumeric_word)
+            word = ''.join(processed_word[j] if char.isalpha() else char for j, char in enumerate(word))
+        processed_words.append(word)
     return ' '.join(processed_words)
 
 
@@ -40,7 +43,7 @@ class TestTextProcessing(unittest.TestCase):
         self.assertEqual(len(word), len(shuffled_word))
         self.assertEqual(word[0], shuffled_word[0])
         self.assertEqual(word[-1], shuffled_word[-1])
-        self.assertCountEqual(list(word), list(shuffled_word))
+        self.assertEqual("epamlxe", shuffled_word)
 
     def test_sort_word(self):
         word = "example"
@@ -48,22 +51,18 @@ class TestTextProcessing(unittest.TestCase):
         self.assertEqual(len(word), len(sorted_word))
         self.assertEqual(word[0], sorted_word[0])
         self.assertEqual(word[-1], sorted_word[-1])
-        self.assertEqual(sorted_word, 'ealmpxe')
+        self.assertEqual('ealmpxe', sorted_word)
 
     def test_process_text_shuffle(self):
-        text = "This is an example text for testing."
+        text = "Hello! This is an example text for testing."
         shuffled_text = process_text(text, shuffle_word)
         self.assertNotEqual(text, shuffled_text)
         self.assertEqual(len(text.split()), len(shuffled_text.split()))
-        self.assertEqual('Tihs', shuffled_text.split()[0])
-        self.assertEqual('tistneg.', shuffled_text.split()[-1])
-        self.assertEqual("Tihs is an eaplmxe txet for tistneg.", shuffled_text)
+        self.assertEqual("Hlelo! Tihs is an eplxame txet for tisteng.", shuffled_text)
 
     def test_process_text_sort(self):
-        text = "This is an example text for testing."
+        text = "Hello! This is an example text for testing."
         sorted_text = process_text(text, sort_word)
         self.assertNotEqual(text, sorted_text)
         self.assertEqual(len(text.split()), len(sorted_text.split()))
-        self.assertEqual(text.split()[0], sorted_text.split()[0])
-        self.assertEqual('teginst.', sorted_text.split()[-1])
-        self.assertEqual("This is an ealmpxe text for teginst.", sorted_text)
+        self.assertEqual("Hello! This is an ealmpxe text for teinstg.", sorted_text)
