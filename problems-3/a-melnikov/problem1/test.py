f7be326e21a65b4fb66bd6c3d55562560937bbf7
Мельников Андрей <a.melnikov4@g.nsu.ru>
2024-05-06 17:01:14 +0700

    Add -> None for tests

--- problems-3/a-melnikov/problem1/test.py
+++ problems-3/a-melnikov/problem1/test.py
@@ -3,13 +3,13 @@ from main import Table
 
 
 class TestTableInit(unittest.TestCase):
-    def test_null(self):
+    def test_null(self) -> None:
         table: Table = Table(3, 15)
         self.assertEqual(table.rows, 3)
         self.assertEqual(table.columns, 15)
         self.assertEqual(table.table, [[None] * 15] * 3)
 
-    def test_common(self):
+    def test_common(self) -> None:
         matrix: list[list[float]] = [[12.3, 13.4, 5.6], [1.5, 2.5, 6.5]]
         table: Table[float] = Table(2, 3, matrix)
         self.assertEqual(table.rows, 2)
@@ -18,7 +18,7 @@ class TestTableInit(unittest.TestCase):
 
 
 class TestTableTail(unittest.TestCase):
-    def test(self):
+    def test(self) -> None:
         table: Table = Table(4, 2, [[1.1, 2.2], [3.3, 4.4], [5.5, 6.6], [7.7, 8.8]])
         expected: list[list[float]] = [[3.3, 4.4], [5.5, 6.6], [7.7, 8.8]]
         result = table.tail(3)
@@ -27,7 +27,7 @@ class TestTableTail(unittest.TestCase):
         self.assertEqual(expected, result.table)
 
 class TestTableHead(unittest.TestCase):
-    def test(self):
+    def test(self) -> None:
         table: Table = Table(4, 2, [[1.1, 2.2], [3.3, 4.4], [5.5, 6.6], [7.7, 8.8]])
         expected: list[list[float]] = [[1.1, 2.2], [3.3, 4.4], [5.5, 6.6]]
         result = table.head(3)
@@ -37,7 +37,7 @@ class TestTableHead(unittest.TestCase):
 
 
 class TestTableSelectRows(unittest.TestCase):
-    def test(self):
+    def test(self) -> None:
         table: Table = Table(4, 2, [[1.1, 2.2], [3.3, 4.4], [5.5, 6.6], [7.7, 8.8]])
         expected: list[list[float]] = [[3.3, 4.4], [7.7, 8.8]]
         result = table.select_rows(1, 3)
@@ -47,7 +47,7 @@ class TestTableSelectRows(unittest.TestCase):
 
 
 class TestTableSelectColumns(unittest.TestCase):
-    def test(self):
+    def test(self) -> None:
         table: Table = Table(2, 4, [[1.1, 2.2, 3.3, 4.4], [5.5, 6.6, 7.7, 8.8]])
         expected: list[list[float]] = [[1.1, 3.3], [5.5, 7.7]]
         result = table.select_columns(0, 2)
@@ -57,7 +57,7 @@ class TestTableSelectColumns(unittest.TestCase):
 
 
 class TestTableJoinRows(unittest.TestCase):
-    def test(self):
+    def test(self) -> None:
         table1: Table = Table(3, 2, [[1.1, 2.2], [3.3, 4.4], [5.5, 6.6]])
         table2: Table = Table(2, 2, [[7.7, 8.8], [9.9, 10.11]])
         expected: list[list[float]] = [[1.1, 2.2], [3.3, 4.4], [5.5, 6.6], [7.7, 8.8], [9.9, 10.11]]
@@ -68,7 +68,7 @@ class TestTableJoinRows(unittest.TestCase):
 
 
 class TestTableJoinColumns(unittest.TestCase):
-    def test(self):
+    def test(self) -> None:
         table1: Table = Table(2, 3, [[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]])
         table2: Table = Table(2, 2, [[7.7, 8.8], [9.9, 10.11]])
         expected: list[list[float]] = [[1.1, 2.2, 3.3, 7.7, 8.8], [4.4, 5.5, 6.6, 9.9, 10.11]]
