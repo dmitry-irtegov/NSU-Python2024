2d906336064a4a1476fa628547c515f5a81f4bd4
nosikmy <91544344+nosikmy@users.noreply.github.com>
2024-05-20 01:04:29 +0700

    exeptions

--- problems-3/a-ramazanova/problem-1.py
+++ problems-3/a-ramazanova/problem-1.py
@@ -11,7 +11,7 @@ class Table:
 
     def _validate_data(self):
         if not isinstance(self._data, list) or not all(isinstance(row, list) for row in self._data):
-            raise ValueError("Table data must be a list of lists")
+            raise TypeError("Table data must be a list of lists")
         if self._data:
             row_len = len(self._data[0])
             if any(len(row) != row_len for row in self._data):
@@ -24,24 +24,15 @@ class Table:
         return Table(self._data[-n:])
 
     def select_rows(self, indices):
-        if any(i >= len(self._data) for i in indices):
-            raise ValueError(f'Id must be < {len(self._data)}')
-        selected_rows = [self._data[i] for i in indices if i < len(self._data)]
-        return Table(selected_rows)
+        return Table([self._data[i] for i in indices if i < len(self._data)])
 
     def append_rows(self, other_table):
-        new_data = self._data + other_table.get_data()
-        return Table(new_data)
+        return Table(self._data + other_table.get_data())
 
     def append_columns(self, other_table):
-        if len(self._data) != len(other_table.get_data()):
-            raise ValueError("Tables must have the same number of rows")
-        combined_data = [self._data[i] + other_table.get_data()[i] for i in range(len(self._data))]
-        return Table(combined_data)
+        return Table([self._data[i] + other_table.get_data()[i] for i in range(len(self._data))])
 
     def select_columns(self, fields):
-        if any(i >= len(self._data[0]) for i in fields):
-            raise ValueError(f'Fields must be < {len(self._data[0])}')
         return Table([[row[i] for i in fields] for row in self._data])
 
     def __str__(self):
@@ -56,12 +47,6 @@ class TestTable(unittest.TestCase):
             [7, 8, 9]
         ]
         self.valid_table = Table(self.valid_data)
-
-        self.invalid_data = [
-            [1, 2, 3],
-            [4, 5],
-            [7, 8, 9]
-        ]
 
     def test_valid_data(self):
         self.assertEqual(self.valid_table._data, self.valid_data)
@@ -120,21 +105,18 @@ class TestTable(unittest.TestCase):
         ]
         self.assertEqual(selected_columns_table._data, expected_data)
 
-    def test_select_invalid_columns(self):
-        with self.assertRaises(ValueError):
-            self.valid_table.select_columns([2, 5])
+    def test_type_error(self):
+        with self.assertRaises(TypeError):
+            Table("dvdkv")
 
-    def test_select_invalid_row(self):
+    def test_value_error(self):
+        invalid_data = [
+            [1, 2, 3],
+            [4, 5],
+            [7, 8, 9]
+        ]
         with self.assertRaises(ValueError):
-            self.valid_table.select_rows([2, 5])
-
-    def test_invalid_data(self):
-        with self.assertRaises(ValueError):
-            Table(self.invalid_data)
-
-    def test_invalid_data2(self):
-        with self.assertRaises(ValueError):
-            Table("sfesf")
+            Table(invalid_data)
 
 
 if __name__ == '__main__':
