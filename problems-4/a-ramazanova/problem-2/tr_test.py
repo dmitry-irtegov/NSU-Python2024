cbe2b6c3ba0c9c9849a9d00e87fe9980992e2a39
nosikmy <91544344+nosikmy@users.noreply.github.com>
2024-05-20 12:21:18 +0700

    Exeption

--- problems-4/a-ramazanova/problem-2/tr_test.py
+++ problems-4/a-ramazanova/problem-2/tr_test.py
@@ -1,45 +1,84 @@
-import unittest
-
-from tr import Tr
-
-
-class TestTranslateLine(unittest.TestCase):
-
-    def test_translate_line_no_replacement(self):
-        self.translator = Tr("aeiou", "12345", "bsz")
-        line = ":DDDDDD"
-        result = self.translator.translate_line(line)
-        self.assertEqual(result, line)
-
-    def test_translate_line_with_replacement(self):
-        self.translator = Tr("aeiou", "12345")
-        line = "Hello World"
-        expected_result = "H2ll4 W4rld"
-        result = self.translator.translate_line(line)
-        self.assertEqual(result, expected_result)
-
-    def test_translate_line_with_deletion(self):
-        self.translator = Tr("", "", "aeiou")
-        line = "Hello World"
-        expected_result = "Hll Wrld"
-        result = self.translator.translate_line(line)
-        self.assertEqual(result, expected_result)
-
-    def test_translate_line_with_both_replacement_and_deletion(self):
-        self.translator = Tr("aeiou", "12345", "l")
-        line = "Hello World"
-        expected_result = "H24 W4rd"
-        result = self.translator.translate_line(line)
-        self.assertEqual(result, expected_result)
-
-    def test_repeated_chars(self):
-        with self.assertRaises(ValueError):
-            self.tr = Tr("dvdv", "abcd")
-
-    def test_different_size(self):
-        with self.assertRaises(ValueError):
-            self.tr = Tr("abcd", "xyz")
+import argparse
+import sys
 
 
-if __name__ == '__main__':
-    unittest.main()
+class Tr:
+    def __init__(self, replace_from, replace_to, delete_chars=None):
+        self._file_path = None
+        if len(replace_from) != len(replace_to):
+            raise ValueError('Characters to replace and replacement characters must be the same length')
+        if len(replace_from) != len(set(replace_from)):
+            raise ValueError('Replacement characters must not be repeated')
+        self._replace_from = replace_from
+        self._replace_to = replace_to
+        self._delete_chars = delete_chars
+
+    def _read_file(self):
+        self._file_path = input("Enter the path to the file: ")
+        try:
+            with open(self._file_path, "r") as file:
+                return file.readlines()
+        except Exception as e:
+            e.additional_info = "An error occurred while reading file"
+            raise e
+
+    def _write_file(self, res):
+        while True:
+            mod = input("Modify this file or create a new one? this/new ")
+            try:
+                if mod == "this":
+                    file = open(self._file_path, 'w')
+                    break
+                elif mod == "new":
+                    file = open(f'tr_{self._file_path}', 'w')
+                    break
+            except Exception as e:
+                e.additional_info = "An error occurred while writing file"
+                raise e
+
+        with file:
+            file.writelines(res)
+
+    def translate_line(self, line):
+        res = ''
+        for c in line:
+            if self._delete_chars and c in self._delete_chars:
+                continue
+            if c in self._replace_from:
+                res += self._replace_to[self._replace_from.index(c)]
+            else:
+                res += c
+        return res
+
+    def translate_file(self):
+        lines = self._read_file()
+        res = [''] * len(lines)
+        for i, line in enumerate(lines):
+            res[i] = self.translate_line(line)
+        self._write_file(res)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("replace_from", help="Characters to replace")
+    parser.add_argument("replace_to", help="Replacement characters")
+    parser.add_argument("-d", "--delete", help="Characters to delete")
+    args = parser.parse_args()
+
+    try:
+        tr = Tr(args.replace_from, args.replace_to, args.delete)
+    except Exception as e:
+        sys.stderr.write(f'Problem with arguments: {e}')
+        exit(1)
+    try:
+        tr.translate_file()
+    except KeyboardInterrupt:
+        sys.stderr.write("The process was interrupted")
+        exit(0)
+    except Exception as e:
+        sys.stderr.write(f'{e.additional_info}: {e}')
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
