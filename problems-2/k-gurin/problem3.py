9ad9b25cf9f69380f47528e6d424cbfb4b6de106
Your Name <you@example.com>
2024-05-27 11:41:45 +0700

    OsError -> Exception

--- problems-2/k-gurin/problem3.py
+++ problems-2/k-gurin/problem3.py
@@ -18,7 +18,7 @@ def get_files(path: str) -> list:
             try:
                 file_size = os.stat(file_path).st_size
                 files.append((file, file_size))
-            except OSError as e:
+            except Exception as e:
                 print(f"Error while getting file info for '{file}': {e}")
 
     return sorted(files, key=lambda item: (-item[1], item[0]))
