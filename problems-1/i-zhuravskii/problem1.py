f37d921dc9db98a1773b8e9b7d63ed38f19a8a6b
buljad <i.zhuravskii@g.nsu.ru>
2024-05-27 11:14:24 +0700

    Add unittest

--- problems-1/i-zhuravskii/problem1.py
+++ problems-1/i-zhuravskii/problem1.py
@@ -1,13 +1,27 @@
-
-inputString = input()
-numbers_list = [int(x) for x in inputString.split()]
+import unittest
 
 
 def cum_sum(numbers):
-    res = [0] * (len(numbers_list) + 1)
+    res = [0] * (len(numbers) + 1)
     for i, number in enumerate(numbers, 1):
         res[i] = number + res[i - 1]
     return res
 
 
-print(cum_sum(numbers_list))
+class TestCumulativeSum(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(cum_sum([]), [0])
+
+    def test_one_number(self):
+        self.assertEqual(cum_sum([3]), [0, 3])
+
+    def test_three_numbers(self):
+        self.assertEqual(cum_sum([1, 2, 3]), [0, 1, 3, 6])
+
+    def test_mixed_numbers(self):
+        self.assertEqual(cum_sum([1, -2, 3]), [0, 1, -1, 2])
+
+
+if __name__ == '__main__':
+    unittest.main()
