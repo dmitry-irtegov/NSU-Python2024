a0e926c61fc188717c40ce3a2a66727db4a16228
Timofey X <vuki03@mail.ru>
2024-05-27 16:20:49 +0700

    [6.1] Add exceptions

--- problems-6/t-khrapovitskii/problem1/main.py
+++ problems-6/t-khrapovitskii/problem1/main.py
@@ -3,20 +3,52 @@ import random
 from letter_shuffler import LetterShuffler
 
 if __name__ == '__main__':
-    with open('text.txt', 'r', encoding='utf-8') as original:
-        with open('shuffled.txt', 'w', encoding='utf-8') as shuffle:
-            shuffler1 = LetterShuffler(random.shuffle, shuffle)
-            while True:
-                char = original.read(1)
-                if char == '':
-                    break
-                shuffler1.give_char(char)
+    try:
+        with open('text.txt', 'r', encoding='utf-8') as original:
+            try:
+                with open('shuffled.txt', 'w', encoding='utf-8') as shuffle:
+                    shuffler1 = LetterShuffler(random.shuffle, shuffle)
+                    while True:
+                        try:
+                            char = original.read(1)
+                        except Exception as e:
+                            print("Error when reading from text.txt\n" + str(e))
+                            exit(1)
+                        if char == '':
+                            break
+                        try:
+                            shuffler1.give_char(char)
+                        except Exception as e:
+                            print("Error when writing to shuffled.txt\n" + str(e))
+                            exit(1)
+            except Exception as e:
+                print("Error when opening shuffled.txt\n" + str(e))
+                exit(1)
 
-        original.seek(0)
-        with open('sorted.txt', 'w', encoding='utf-8') as sort:
-            shuffler2 = LetterShuffler(lambda x: x.sort(), sort)
-            while True:
-                char = original.read(1)
-                if char == '':
-                    break
-                shuffler2.give_char(char)
+            try:
+                original.seek(0)
+            except Exception as e:
+                print("Error when seeking text.txt\n" + str(e))
+                exit(1)
+            try:
+                with open('sorted.txt', 'w', encoding='utf-8') as sort:
+                    shuffler2 = LetterShuffler(lambda x: x.sort(), sort)
+                    while True:
+                        try:
+                            char = original.read(1)
+                        except Exception as e:
+                            print("Error when reading from text.txt\n" + str(e))
+                            exit(1)
+                        if char == '':
+                            break
+                        try:
+                            shuffler2.give_char(char)
+                        except Exception as e:
+                            print("Error when writing to sorted.txt\n" + str(e))
+                            exit(1)
+            except Exception as e:
+                print("Error when opening sorted.txt\n" + str(e))
+                exit(1)
+    except Exception as e:
+        print("Error when opening text.txt\n" + str(e))
+        exit(1)
