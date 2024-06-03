73b48a7e6f8b4e5009e474e720ffac7893167e04
Mikhail <evdokimm05@gmail.com>
2024-06-03 11:57:23 +0700

    pr6_2 init

---
+++ problems-6/m-evdokimov/problem-2/url finder.py
@@ -0,0 +1,39 @@
+import unittest
+import re
+
+def find_url(line, urls):
+    for match in re.finditer(r'www\.[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,6}', line):
+        end = line[match.end():].find(' ')
+        if end == -1:
+            path = line[match.end():]
+        else:
+            path = line[match.end():match.end()+end]
+        urls.append(match[0] + path)
+    return urls
+        
+
+def url_finder(filename):
+    urls =[]
+    file = open(filename, 'r')
+    for line in file:
+        find_url(line, urls)
+    file.close()
+    return urls
+
+class TestUrls(unittest.TestCase):
+  
+    def test_simple(self):
+        self.assertEqual(url_finder("input1.txt"), ['www.omim.org/entry/227220', 'www.kinopoisk1.ru/', 'www.kinopoisk2.ru/'])
+    
+    def test_atTheEndOfLine(self):
+        self.assertEqual(url_finder("input2.txt"), ['www.omim.org/entry/227220'])
+        
+    def test_noLink(self):
+        self.assertEqual(url_finder("input3.txt"), [])
+        
+    def test_badLink(self):
+        self.assertEqual(url_finder("input4.txt"), [])
+        
+    
+if __name__ == "__main__":
+  unittest.main()
