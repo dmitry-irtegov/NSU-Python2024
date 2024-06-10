69520c1df1f2b8cac1653b66ab2e5e7cf94f2fde
Timofey X <vuki03@mail.ru>
2024-06-10 16:03:27 +0700

    [9.2] Fixes

--- problems-9/t-khrapovitskii/problem2/storage.py
+++ problems-9/t-khrapovitskii/problem2/storage.py
@@ -1,6 +1,4 @@
-import time
 from collections.abc import Mapping
-from itertools import chain
 from threading import Lock
 # from multiprocessing import Lock ?
 from typing import Any
@@ -11,40 +9,23 @@ from storage_transaction import StorageTransaction
 class Storage(Mapping):
     def __init__(self, initial: dict[Any, Any] | None = None):
         if initial is None:
-            self._dict = {}
+            self._dict: tuple[dict[Any, Any], dict[Any, int]] = ({}, {})
         else:
-            self._dict = initial.copy()
-        self._key_modifications: dict[Any, int] = {i: 0 for i in self._dict}
+            self._dict = (initial.copy(), {i: 0 for i in initial})
         self._last_modification: int = 0
         self._update_lock = Lock()
 
     def __getitem__(self, __key):
-        return self._dict[__key]
+        return self._dict[0][__key]
 
     def __len__(self):
-        return len(self._dict)
+        return len(self._dict[0])
 
     def __iter__(self):
-        return iter(self._dict)
+        return iter(self._dict[0])
 
     def edit(self) -> StorageTransaction:
         return StorageTransaction(self)
-
-    def get_key_modification_time(self, key: Any) -> int | None:
-        return self._key_modifications.get(key, None)
-
-    def update(self, transaction_start_time: int, changes: dict[Any, Any], deleted: set[Any]) -> None:
-        with self._update_lock:
-            for k in chain(changes.keys(), deleted):
-                if self._key_modifications.get(k, 0) > transaction_start_time:
-                    raise RuntimeError(f'value of key {repr(k)} externally modified during transaction')
-            ns = time.time_ns()
-            for k, v in changes.items():
-                self._dict[k] = v
-                self._key_modifications[k] = ns
-            for k in deleted:
-                self._dict.pop(k, None)
-                self._key_modifications[k] = ns
 
     def __repr__(self):
         return repr(self._dict)
--- problems-9/t-khrapovitskii/problem2/storage_transaction.py
+++ problems-9/t-khrapovitskii/problem2/storage_transaction.py
@@ -1,6 +1,7 @@
 from collections.abc import MutableMapping
 from contextlib import AbstractContextManager
 import time
+from itertools import chain
 from typing import Any, TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -13,14 +14,15 @@ class StorageTransaction(MutableMapping, AbstractContextManager):
         self._storage = storage
         self._changed: dict[Any, Any] = {}
         self._deleted: set[Any] = set()
-        self._orig_dict = storage._dict
+        self._orig_dict = storage._dict[0]
         self._cancel = False
         self._transaction_start = -1
         self._orig_keys: set[Any] = set()
 
+    # noinspection PyProtectedMember
     def _check_modification_time(self, key):
-        modification_time = self._storage.get_key_modification_time(key)
-        if modification_time and modification_time > self._transaction_start:
+        modification_time = self._storage._dict[1].get(key, 0)
+        if modification_time > self._transaction_start:
             raise RuntimeError(f'value of key {repr(key)} externally modified during transaction')
 
     def __getitem__(self, __key) -> Any:
@@ -49,17 +51,35 @@ class StorageTransaction(MutableMapping, AbstractContextManager):
     def __iter__(self):
         return iter(self._orig_keys | self._changed.keys() - self._deleted)
 
+    # noinspection PyProtectedMember
     def __enter__(self):
         self._transaction_start = time.time_ns()
-        # noinspection PyProtectedMember
-        self._orig_keys = set(self._storage._dict.keys())
+        self._orig_keys = set(self._storage._dict[0].keys())
         return self
+
+    # noinspection PyProtectedMember
+    @staticmethod
+    def _update_storage(st: "Storage", transaction_start_time: int, changes: dict[Any, Any], deleted: set[Any]):
+        with st._update_lock:
+            for k in chain(changes.keys(), deleted):
+                if st._dict[1].get(k, 0) > transaction_start_time:
+                    raise RuntimeError(f'value of key {repr(k)} externally modified during transaction')
+            ns = time.time_ns()
+            old = st._dict
+            new_dict = (old[0].copy(), old[1].copy())
+            for k, v in changes.items():
+                new_dict[0][k] = v
+                new_dict[1][k] = ns
+            for k in deleted:
+                new_dict[0].pop(k, None)
+                new_dict[1][k] = ns
+            st._dict = new_dict
 
     def __exit__(self, __exc_type, __exc_value, __traceback):
         transaction_start = self._transaction_start
         self._transaction_start = -2
         if not self._cancel and __exc_type is None:
-            self._storage.update(transaction_start, self._changed, self._deleted)
+            self._update_storage(self._storage, transaction_start, self._changed, self._deleted)
         self._changed = {}
         self._deleted = set()
         self.orig_keys = set()
