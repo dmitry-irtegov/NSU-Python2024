8b8e782e1ec27172d840a1fa62ea2810d49f78b6
Timofey Khrapovitskiy <40120650+cjvth@users.noreply.github.com>
2024-06-10 15:31:26 +0700

    Problems 2018-2 / 2 and 3 (#279)
    * [2018-2.2] Add vector class with tests
    
    * [2018-2.2] Add tests for slice getitem and setitem
    
    * [2018-2.3] Add docstrings of numpydoc format
    
    * [2018-2.2] Fix mypy errors
    
    * [2018-2.2] Move code to add, sub and mul to separate functions

---
+++ problems-2018-2/t-khrapovitskii/problem2/test_vector.py
@@ -0,0 +1,126 @@
+from typing import Sequence
+
+from vector import Vector
+
+
+def compare(a: Vector, b: Sequence[float]):
+    assert len(a) == len(b)
+    for act, exp in zip(a, b):
+        assert act == exp
+
+
+def compare_delta(a: Vector, b: Sequence[float], delta=0.000000001):
+    assert len(a) == len(b)
+    for act, exp in zip(a, b):
+        assert -delta < act - exp < delta
+
+
+class TestVector:
+    def test_len_constructor(self):
+        v = Vector(6)
+        assert len(v) == 6
+        for i in v:
+            assert i == 0
+
+    def test_value_constructor(self):
+        v = Vector([1, 2, 3, 5])
+        assert len(v) == 4
+        assert v[0] == 1
+        assert v[1] == 2
+        assert v[2] == 3
+        assert v[3] == 5
+
+    def test_setitem(self):
+        arr = [1, 2, 3, 5]
+        v = Vector(arr)
+        compare(v, [1, 2, 3, 5])
+        v[2] = 10
+        compare(v, [1, 2, 10, 5])
+        assert arr[2] == 3
+
+    def test_getitem_slice(self):
+        v = Vector([1, 2, 3, 5])
+        assert tuple(v[:2]) == (1, 2)
+
+    def test_setitem_slice(self):
+        v = Vector([1, 2, 3, 5])
+        v[1:4] = [9, 8, 7]
+        compare(v, [1, 9, 8, 7])
+
+    def test_clone(self):
+        v = Vector([1, 2])
+        clone = v.clone()
+        assert len(clone) == 2
+        v[1] = 9
+        assert clone[1] == 2
+
+    def test_iadd(self):
+        v = Vector([10, 20, 30])
+        v_save = v
+        another = Vector([1, 2, 3])
+        v += another
+        compare(v, [11, 22, 33])
+        compare(v_save, [11, 22, 33])
+        compare(another, [1, 2, 3])
+
+    def test_add(self):
+        v = Vector([10, 20, 30])
+        another = Vector([1, 2, 3])
+        res = v + another
+        compare(res, [11, 22, 33])
+        compare(v, [10, 20, 30])
+        compare(another, [1, 2, 3])
+
+    def test_isub(self):
+        v = Vector([10, 20, 30])
+        v_save = v
+        another = Vector([1, 2, 3])
+        v -= another
+        compare(v, [9, 18, 27])
+        compare(v_save, [9, 18, 27])
+        compare(another, [1, 2, 3])
+
+    def test_sub(self):
+        v = Vector([10, 20, 30])
+        another = Vector([1, 2, 3])
+        res = v - another
+        compare(res, [9, 18, 27])
+        compare(v, [10, 20, 30])
+        compare(another, [1, 2, 3])
+
+    def test_imul(self):
+        v = Vector([10, 20, 30])
+        v_save = v
+        scalar = 5.01
+        v *= scalar
+        compare_delta(v, [50.1, 100.2, 150.3])
+        compare_delta(v_save, [50.1, 100.2, 150.3])
+
+    def test_mul(self):
+        v = Vector([10, 20, 30])
+        scalar = 1000
+        res = v * scalar
+        compare(res, [10000, 20000, 30000])
+        compare(v, [10, 20, 30])
+
+    def test_neg(self):
+        v = Vector([10, 20, 30])
+        res = -v
+        compare(res, [-10, -20, -30])
+        compare(v, [10, 20, 30])
+
+    def test_eq(self):
+        v = Vector([10, 20, 30])
+        another = v.clone()
+        assert v == another
+        another[0], another[1] = another[1], another[0]
+        assert v != another
+
+    def test_dot(self):
+        a = Vector([1, 4, 6])
+        b = Vector([10, 11, 18])
+        assert a.dot(b) == 162
+
+    def test_abs(self):
+        v = Vector([3, 12, 10, 6])
+        assert abs(v) == 17
---
+++ problems-2018-2/t-khrapovitskii/problem2/vector.py
@@ -0,0 +1,377 @@
+"""
+Provides a Vector object.
+"""
+
+import math
+from typing import overload, Sequence, Self, SupportsFloat, Iterable
+
+
+class Vector(Sequence):
+    """
+    Class representing a coordinate vector.
+
+    This object contains N floating point numbers that represent the coordinates.
+    The size is set at initialization and cannot be changed.
+
+    Parameters
+    ----------
+    arg : int or Sequence[SupportsFloat]
+        If arg is int, create vector of x zeroes.
+        If arg is Sequence[SupportsFloat], create vector from contents of x.
+    """
+    _value: list[float]
+
+    @overload
+    def __init__(self, size: int):
+        """
+        Create a vector of zeroes.
+
+        Parameters
+        ----------
+        size : int
+            Number of coordinates in the vector.
+        """
+        ...
+
+    @overload
+    def __init__(self, value: Iterable[SupportsFloat]):
+        """
+        Create a vector of given elements.
+
+        Parameters
+        ----------
+        value : Sequence[SupportsFloat]
+            Elements that will be the coordinates of the vector.
+        """
+        ...
+
+    def __init__(self, arg):
+        """
+        Create a vector. This constructor is overloaded.
+
+        Parameters
+        ----------
+        arg : int or Sequence[SupportsFloat]
+            If arg is int, create vector of arg zeroes.
+            If arg is Sequence[SupportsFloat], create vector from contents of arg.
+        """
+        if isinstance(arg, int):
+            self._value = [0] * arg
+        elif isinstance(arg, Sequence):
+            self._value = [float(i) for i in arg]
+        else:
+            raise TypeError(f'constructor argument must be an integer or a sequence, not \'{type(arg)}\'')
+
+    @overload
+    def __getitem__(self, index: int) -> float:
+        """
+        Get ith element of the vector.
+
+        Parameters
+        ----------
+        index : int
+            Index of the coordinate to be returned.
+
+        Returns
+        -------
+        float
+            The ith coordinate of the vector.
+        """
+        ...
+
+    @overload
+    def __getitem__(self, index: slice) -> Sequence[float]:
+        """
+        Get range of elements of the vector.
+
+        Parameters
+        ----------
+        index : slice
+            Range of the coordinate to be returned.
+
+        Returns
+        -------
+        float
+            The ith coordinate of the vector.
+        """
+        ...
+
+    def __getitem__(self, index: int | slice) -> float | Sequence[float]:
+        """
+        Get one or multiple elements of the vector.
+
+        Parameters
+        ----------
+        index : int or slice
+            Index of the coordinate to be returned.
+
+        Returns
+        -------
+        float or Sequence[float]
+            One or multiple coordinates of the vector and index.
+        """
+        return self._value[index]
+
+    @overload
+    def __setitem__(self, index: int, value: float) -> None:
+        """
+        Set one or multiple elements of the vector.
+
+        Parameters
+        ----------
+        index : int
+            Index of the coordinate to set.
+        value : float
+            Value to set at this coordinate.
+        """
+        ...
+
+    @overload
+    def __setitem__(self, index: slice, value: Iterable[float]) -> None:
+        """
+        Set one or multiple elements of the vector.
+
+        Parameters
+        ----------
+        index : int
+            Index of the coordinate to set.
+        value : float
+            Value to set at this coordinate.
+        """
+        ...
+
+    def __setitem__(self, index, value) -> None:
+        """
+        Set one or multiple elements of the vector.
+
+        Parameters
+        ----------
+        index : int or slice
+            Range of the coordinates to set.
+        value : float | Iterable[float]
+            Values to set at these coordinates.
+        """
+        self._value[index] = value
+
+    def __len__(self) -> int:
+        """
+        Get number of coordinates in the vector.
+
+        Returns
+        -------
+        int
+            Number of coordinates in the vector.
+        """
+        return len(self._value)
+
+    def clone(self) -> Self:
+        """
+        Clone the vector so that the copy can be mutated without changing the original.
+
+        Returns
+        -------
+        Vector
+            A clone of this vector.
+        """
+        return self.__class__(self._value)
+
+    def __repr__(self) -> str:
+        """
+        Return a string containing a printable representation of the vector that can be used in eval().
+
+        Returns
+        -------
+        str
+            Representation of the vector.
+        """
+        return f'Vector({repr(self._value)})'
+
+    def __str__(self) -> str:
+        """
+        Return a string containing the nicely printable string representation of the vector.
+
+        Returns
+        -------
+        str
+            Representation of the vector.
+        """
+        return f'Vector[{', '.join(map(str, self._value))}]'
+
+    @staticmethod
+    def _add(s1: list[float], s2: list[float], d: list[float]):
+        for i in range(len(d)):
+            d[i] = s1[i] + s2[i]
+
+    @staticmethod
+    def _sub(s1: list[float], s2: list[float], d: list[float]):
+        for i in range(len(d)):
+            d[i] = s1[i] - s2[i]
+
+    @staticmethod
+    def _mul(s: list[float], scalar: float, d: list[float]):
+        for i in range(len(d)):
+            d[i] = s[i] * scalar
+
+    def __iadd__(self, other: Self) -> Self:
+        """
+        Increase vector by another vector.
+
+        Parameters
+        ----------
+        other : Vector
+            Another vector to add.
+
+        Returns
+        -------
+        Vector
+            Reference to self.
+        """
+        self._add(self._value, other._value, self._value)
+        return self
+
+    def __add__(self, other: Self) -> Self:
+        """
+        Get the sum of two vectors.
+
+        Parameters
+        ----------
+        other : Vector
+            Another vector to add.
+
+        Returns
+        -------
+        Vector
+            A new vector containing sum of two vectors.
+        """
+        ret = self.__class__(len(self))
+        self._add(self._value, other._value, ret._value)
+        return ret
+
+    def __isub__(self, other: Self) -> Self:
+        """
+        Decrease vector by another vector.
+
+        Parameters
+        ----------
+        other : Vector
+            Another vector to subtract.
+
+        Returns
+        -------
+        Vector
+            Reference to self.
+        """
+        self._sub(self._value, other._value, self._value)
+        return self
+
+    def __sub__(self, other: Self) -> Self:
+        """
+        Get the difference of two vectors.
+
+        Parameters
+        ----------
+        other : Vector
+            Another vector to subtract.
+
+        Returns
+        -------
+        Vector
+            A new vector containing difference of two vectors.
+        """
+        ret = self.__class__(len(self))
+        self._sub(self._value, other._value, ret._value)
+        return ret
+
+    def __imul__(self, scalar: SupportsFloat) -> Self:
+        """
+        Multiply the vector by a scalar.
+
+        Parameters
+        ----------
+        scalar : float
+            Scalar value to multiply the vector by.
+
+        Returns
+        -------
+        Vector
+            Reference to self.
+        """
+        self._mul(self._value, float(scalar), self._value)
+        return self
+
+    def __mul__(self, scalar: SupportsFloat) -> Self:
+        """
+        Get the vector multiplied by a scalar.
+
+        Parameters
+        ----------
+        scalar : float
+            Scalar value to multiply the vector by.
+
+        Returns
+        -------
+        Vector
+            A new vector containing this vector multiplied by a scalar.
+        """
+        ret = self.__class__(len(self))
+        self._mul(self._value, float(scalar), ret._value)
+        return ret
+
+    def __neg__(self) -> Self:
+        """
+        Get the negative of the vector.
+
+        Returns
+        -------
+        Vector
+            A new negated vector.
+        """
+        return self * -1
+
+    def __eq__(self, other: object) -> bool:
+        """
+        Compare two vectors.
+
+        Parameters
+        ----------
+        other : Vector
+            Another vector to compare.
+
+        Returns
+        -------
+        bool
+            True if the vectors are equal, False otherwise.
+        """
+        if not isinstance(other, Vector):
+            return False
+        for i in zip(self._value, other._value):
+            if i[0] != i[1]:
+                return False
+        return True
+
+    def dot(self, other: Self) -> float:
+        """
+        Compute the dot product of two vectors.
+
+        Parameters
+        ----------
+        other : Vector
+            Another vector to compute dot product.
+
+        Returns
+        -------
+        float
+            The dot product of the vectors.
+        """
+        return sum(map(lambda t: t[0] * t[1], zip(self._value, other._value)))
+
+    def __abs__(self) -> float:
+        """
+        Compute the Euclidean norm of the vector.
+
+        Returns
+        -------
+        float
+            The Euclidean norm of the vector.
+        """
+        return math.sqrt(self.dot(self))
