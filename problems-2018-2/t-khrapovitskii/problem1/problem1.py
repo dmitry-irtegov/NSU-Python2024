1b045ec2b3e5771af4133deb5835f7551cddcf64
Timofey Khrapovitskiy <40120650+cjvth@users.noreply.github.com>
2024-06-14 13:47:26 +0700

    Problem-2018-2-1 (#287)
    * [2018-2.1] Solve problem
    
    * [2018-2.1] Remove some char substitution

---
+++ problems-2018-2/t-khrapovitskii/problem1/problem1.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+
+def convert_complex(s: str) -> complex:
+    return complex(s)
+
+
+def is_complex(s: str) -> bool:
+    try:
+        convert_complex(s)
+        return True
+    except ValueError:
+        return False
+
+
+if __name__ == '__main__':
+    for line in stdin:
+        try:
+            converted = convert_complex(line)
+            print('Got complex number:', converted)
+            break
+        except ValueError:
+            print('Not a number')
